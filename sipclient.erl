d2756310b51b117eef0a66fd9cefc077725bc027
Magnus Ahltorp <map@kth.se>
2002-12-09 22:45:08 +0000

    *** empty log message ***
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@25 e454d3dc-12ff-0310-9b8b-94264392205f

---
+++ sipclient.erl
@@ -0,0 +1,30 @@
+-module(sipclient).
+-export([dial/2]).
+
+dial(Number, Dest) ->
+    {ok, Socket} = gen_udp:open(0),
+    Body = sdp:print(Dest),
+    From = "\"9532\" <sip:9532@nada.kth.se>",
+    To = "sip:" ++ Number ++ "@kth.se",
+    {Megasec, Sec, Microsec} = now(),
+    CallID = "2b6fb0320-" ++
+	integer_to_list(Megasec) ++ "-" ++
+	integer_to_list(Sec) ++ "-" ++
+	integer_to_list(Microsec) ++ "-" ++
+	"-23b6b0-2e3303331@" ++ siphost:myip(),
+    Sendheader = [{"via", []},
+		  {"From", [From]},
+		  {"To", [To]},
+		  {"Call-ID", [CallID]},
+		  {"Content-Type", ["application/sdp"]},
+		  {"Content-Length", [integer_to_list(length(Body))]},
+		  {"CSeq", ["101 INVITE"]},
+		  {"Expires", ["3600"]}],
+    Url = sipurl:parse("sip:" ++ Number ++ "@kth.se"),
+    {atomic, ok} =  phone:insert_call_unique(CallID, Sendheader, self()),
+    siprequest:send_proxy_request(Sendheader,
+				  Socket,
+				  {"INVITE",
+				   Url,
+				   Body}),
+    true.
