5f4d77a63490325df896f35b1f1b757c8d0d9b12
Magnus Ahltorp <map@kth.se>
2003-06-02 18:31:07 +0000

    From ft@it.su.se: logging changes. Check pstngateway name in To.
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@194 e454d3dc-12ff-0310-9b8b-94264392205f

--- pstnproxy.erl
+++ pstnproxy.erl
@@ -26,16 +26,26 @@ routeRequestToPSTN(FromIP, ToHost) ->
 		    logger:log(debug, "Routing: Source IP ~s is not PSTN gateway and ~p is me, route to PSTN gateway", [FromIP, ToHost]),
 		    true;
 		_ ->
-		    logger:log(debug, "Routing: Denied request from ~s to ~s - not my problem", [FromIP, ToHost]),
-		    nomatch
+		    case pstngateway(ToHost) of
+			true ->
+			    logger:log(debug, "Routing: Destination (~p) is PSTN gateway", [ToHost]),
+			    true;
+			_ ->
+			    logger:log(debug, "Routing: Denied request from ~s to ~s - not my problem", [FromIP, ToHost]),
+			    nomatch
+		    end
 	    end
     end.
 
 request(Method, URL, Header, Body, Socket, FromIP) ->
     {User, Pass, Host, Port, Parameters} = URL,
-    logger:log(normal, "~s ~s", [Method, sipurl:print(URL)]),
+    logger:log(debug, "~s ~s", [Method, sipurl:print(URL)]),
+    {_, FromURI} = sipheader:to(keylist:fetch("From", Header)),
+    {_, ToURI} = sipheader:to(keylist:fetch("To", Header)),
+    LogStr = io_lib:format("~s ~s [client=~s, from=<~s>, to=<~s>]", [Method, sipurl:print(URL), FromIP, sipurl:print(FromURI), sipurl:print(ToURI)]),
     case routeRequestToPSTN(FromIP, Host) of
 	true ->
+	    logger:log(normal, "~s -> PSTN gateway", [LogStr]),
 	    case Method of
 		"INVITE" ->
 		    toPSTNrequest(Method, User, Header, Body, Socket);
@@ -51,6 +61,7 @@ request(Method, URL, Header, Body, Socket, FromIP) ->
 		    siprequest:send_result(Header, Socket, "", 501, "Not Implemented")
 	    end;
 	false ->
+	    logger:log(normal, "~s -> SIP server", [LogStr]),
 	    toSIPrequest(Method, URL, Header, Body, Socket);
 	_ ->
 	    siprequest:send_result(Header, Socket, "", 403, "Forbidden")
@@ -59,15 +70,20 @@ request(Method, URL, Header, Body, Socket, FromIP) ->
 toSIPrequest(Method, URL, Header, Body, Socket) ->
     {User, Pass, Host, Port, Parameters} = URL,
     Newlocation = {User, none, sipserver:get_env(sipproxy), none, []},
-    Route = "<" ++ sipurl:print({User, Pass, Host, Port,
-				 ["maddr=" ++ siphost:myip()]}) ++ ">",
-    Newheaders = keylist:append({"Record-route", Route}, Header),
-    siprequest:send_proxy_request(Newheaders, Socket, {Method, Newlocation, Body, []}).
+    case Newlocation of
+	none ->
+	    none;
+	_ ->
+	    Route = "<" ++ sipurl:print({User, Pass, lists:nth(1, sipserver:get_env(myhostnames)), Port,
+    				["maddr=" ++ siphost:myip()]}) ++ ">",
+	    Newheaders = keylist:prepend({"Record-Route", Route}, Header),
+	    siprequest:send_proxy_request(Newheaders, Socket, {Method, Newlocation, Body, []})
+    end.
 
 toPSTNrequest(Method, Phone, Header, Body, Socket) ->
     Newlocation = {Phone, none, lists:nth(1, sipserver:get_env(pstngatewaynames)), none, []},
     {_, FromURI} = sipheader:to(keylist:fetch("From", Header)),
-    {Fromphone, _, _, _, _} = FromURI,
+    Fromphone = local:sipuser(FromURI),
     Classdefs = sipserver:get_env(classdefs, [{"", unknown}]),
     Route = "<" ++ sipurl:print({Phone, none, lists:nth(1, sipserver:get_env(myhostnames)), none,
 				 ["maddr=" ++ siphost:myip()]}) ++ ">",
