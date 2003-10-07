305eb1128d508023aae3b2e405c6f5515dbb6d2c
Fredrik Thulin <ft@it.su.se>
2003-10-07 11:59:26 +0000

    init
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@284 e454d3dc-12ff-0310-9b8b-94264392205f

---
+++ bootstrap.erl
@@ -0,0 +1,12 @@
+-module(bootstrap).
+-export([start/1]).
+
+start([AdminPassword]) ->
+    io:format("Bootstrapping Yxa on node ~p~n", [node()]),
+    ok = mnesia:create_schema([node()]),
+    ok = mnesia:start(),
+    io:format("Creating tables on this Mnesia node (~p)~n", [node()]),
+    phone:create([node()]),
+    database_regexproute:create([node()]),
+    io:format("Creating admin-user~n"),
+    phone:insert_user("admin", AdminPassword, [admin], []).
