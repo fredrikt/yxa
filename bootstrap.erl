8830225104078ad436538d8cd8750a9d5bcd2294
Fredrik Thulin <ft@it.su.se>
2004-01-29 11:10:03 +0000

    create forward and call databases
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@297 e454d3dc-12ff-0310-9b8b-94264392205f

--- bootstrap.erl
+++ bootstrap.erl
@@ -8,5 +8,7 @@ start([AdminPassword]) ->
     io:format("Creating tables on this Mnesia node (~p)~n", [node()]),
     phone:create([node()]),
     database_regexproute:create([node()]),
+    database_forward:create([node()]),
+    database_call:create([node()]),
     io:format("Creating admin-user~n"),
     phone:insert_user("admin", AdminPassword, [admin], []).
