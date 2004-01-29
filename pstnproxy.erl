c7a4ac3b74ea5301e598dede08c8a64390640e5e
Fredrik Thulin <ft@it.su.se>
2004-01-29 13:40:11 +0000

    merge with SU_V2_1: stateless handling of ACK messages generated at this proxy, sending of challenges are now handled from here instead of by sipauth, only add Remote- Party-Id to INVITE.
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@325 e454d3dc-12ff-0310-9b8b-94264392205f

--- pstnproxy.erl
+++ pstnproxy.erl
@@ -3,7 +3,7 @@
 
 start(normal, Args) ->
     Pid = spawn(sipserver, start, [fun init/0, fun request/6,
-				   fun response/6, [user, numbers], false]),
+				   fun response/6, [user, numbers]]),
     {ok, Pid}.
 
 init() ->
@@ -21,41 +21,66 @@ routeRequestToPSTN(FromIP, ToHost) ->
 	    logger:log(debug, "Routing: Source IP ~s is PSTN gateway, route to SIP proxy", [FromIP]),
 	    false;		
 	_ ->
-	    case localhostname(ToHost) of
-		true ->
-		    logger:log(debug, "Routing: Source IP ~s is not PSTN gateway and ~p is me, route to PSTN gateway", [FromIP, ToHost]),
+	    IsLocalHostname = localhostname(ToHost),
+	    IsPstnGatewayHostname = pstngateway(ToHost),
+	    if
+		IsLocalHostname == true ->
+		    logger:log(debug, "Routing: Source IP ~s is not PSTN gateway and ~p is me, route to PSTN gateway",
+		    		[FromIP, ToHost]),
 		    true;
-		_ ->
-		    case pstngateway(ToHost) of
-			true ->
-			    logger:log(debug, "Routing: Destination (~p) is PSTN gateway", [ToHost]),
-			    true;
-			_ ->
-			    logger:log(debug, "Routing: Denied request from ~s to ~s - not my problem", [FromIP, ToHost]),
-			    nomatch
-		    end
+		IsPstnGatewayHostname == true ->
+		    logger:log(debug, "Routing: Source IP ~s is not PSTN gateway and ~p is me, route to PSTN gateway",
+				[FromIP, ToHost]),
+		    true;
+		true ->
+		    logger:log(debug, "Routing: Denied request from ~s to ~s - not my problem",[FromIP, ToHost]),
+		    nomatch
 	    end
     end.
 
-request(Method, URI, Header, Body, Socket, FromIP) ->
+request("ACK", URL, Header, Body, Socket, FromIP) ->
+    logger:log(debug, "ACK ~p", [sipurl:print(URL)]),
+    ToTag = sipheader:get_tag(keylist:fetch("To", Header)),
+    case ToTag of
+	"z9hG4bK-yxa-" ++ Foo ->
+	    case lists:suffix(siprequest:myhostname() ++ "-drop-ACK", Foo) of
+		true ->
+		    logger:log(debug, "Dropping ACK of my challenge"),
+		    LogStr = sipserver:make_logstr({request, "ACK", URL, Header, Body}, FromIP),
+		    logger:log(debug, "~s -> dropped", [LogStr]);
+		false ->
+		    do_request("ACK", URL, Header, Body, Socket, FromIP)
+	    end;
+	_ ->
+	    do_request("ACK", URL, Header, Body, Socket, FromIP)
+    end;
+
+request(Method, URL, Header, Body, Socket, FromIP) ->
+    do_request(Method, URL, Header, Body, Socket, FromIP).
+
+do_request(Method, URI, Header, Body, Socket, FromIP) ->
     {User, Pass, Host, Port, Parameters} = URI,
     LogStr = sipserver:make_logstr({request, Method, URI, Header, Body}, FromIP),
     case routeRequestToPSTN(FromIP, Host) of
 	true ->
-	    logger:log(normal, "~s -> PSTN gateway", [LogStr]),
+	    logger:log(debug, "~s -> PSTN gateway", [LogStr]),
 	    case Method of
 		"INVITE" ->
-		    toPSTNrequest(Method, URI, Header, Body, Socket);
+		    toPSTNrequest(Method, URI, Header, Body, Socket, LogStr);
 		"ACK" ->
-		    toPSTNrequest(Method, URI, Header, Body, Socket);
+		    toPSTNrequest(Method, URI, Header, Body, Socket, LogStr);
 		"PRACK" ->
-		    toPSTNrequest(Method, URI, Header, Body, Socket);
+		    toPSTNrequest(Method, URI, Header, Body, Socket, LogStr);
 		"CANCEL" ->
-		    toPSTNrequest(Method, URI, Header, Body, Socket);
+		    toPSTNrequest(Method, URI, Header, Body, Socket, LogStr);
 		"BYE" ->
-		    toPSTNrequest(Method, URI, Header, Body, Socket);
+		    toPSTNrequest(Method, URI, Header, Body, Socket, LogStr);
+		"OPTIONS" ->
+		    toPSTNrequest(Method, URI, Header, Body, Socket, LogStr);
 		_ ->
-		    siprequest:send_result(Header, Socket, "", 501, "Not Implemented")
+		    logger:log(normal, "~s -> PSTN denied, 405 Method Not Allowed", [LogStr]),
+		    siprequest:send_result(Header, Socket, "", 405, "Method Not Allowed")
+		 %   	[{"Allow, ["INVITE", "ACK", "PRACK", "CANCEL", "BYE", "OPTIONS"]}])
 	    end;
 	false ->
 	    logger:log(normal, "~s -> SIP server", [LogStr]),
@@ -67,7 +92,6 @@ request(Method, URI, Header, Body, Socket, FromIP) ->
 
 toSIPrequest(Method, URI, Header, Body, Socket) ->
     {User, Pass, Host, Port, Parameters} = URI,
-    {_, FromURI} = sipheader:to(keylist:fetch("From", Header)),
     Newlocation = case localhostname(Host) of
 	true ->
 	    logger:log(debug, "Performing ENUM lookup on ~p", [User]),
@@ -99,61 +123,122 @@ toSIPrequest(Method, URI, Header, Body, Socket) ->
 	        false -> Header
 	    end,
 	    {_, _, DstHost, _, _} = Newlocation,
-	    Newheaders2 = add_caller_identity_for_sip(Newheaders, FromURI, DstHost),
-	    siprequest:send_proxy_request(Newheaders2, Socket, {Method, Newlocation, Body, []})
+	    {_, FromURI} = sipheader:to(keylist:fetch("From", Header)),
+	    Newheaders2 = add_caller_identity_for_sip(Method, Newheaders, FromURI),
+	    siprequest:send_proxy_request(Socket, {Method, URI, Newheaders2, Body}, Newlocation, [])
     end.
 
-toPSTNrequest(Method, URI, Header, Body, Socket) ->
-    {Phone, _, _, _, _} = URI,
-    PSTNgateway = lists:nth(1, sipserver:get_env(pstngatewaynames)),
-    Newlocation = {Phone, none, PSTNgateway, none, []},
-    {_, FromURI} = sipheader:to(keylist:fetch("From", Header)),
-    Fromphone = local:sipuser(FromURI),
-    Classdefs = sipserver:get_env(classdefs, [{"", unknown}]),
-    Newheaders = case sipserver:get_env(record_route, true) of
-	true -> siprequest:add_record_route(Header);
-        false -> Header
+toPSTNrequest(Method, URI, Header, Body, Socket, LogStr) ->
+    {DstNumber, _, ToHost, ToPort, _} = URI,
+    {NewURI, NewHeaders1} = case localhostname(ToHost) of
+	true ->
+	    case local:lookuppstn(DstNumber) of
+		{proxy, Dst} ->
+		    {Dst, Header};	
+		_ ->
+		    % Route to default PSTN gateway
+		    PSTNgateway1 = lists:nth(1, sipserver:get_env(pstngatewaynames)),
+		    NewURI2 = {DstNumber, none, PSTNgateway1, none, []},
+		    {NewURI2, Header}
+	    end;
+	_ ->
+	    %AddRoute = sipheader:contact_print([{none, {none, none, ToHost, ToPort, ["lr=true"]}}]),
+	    %NewHeader1 = keylist:prepend({"Route", AddRoute}, Header),
+	    {URI, Header}
     end,
-    Newheaders2 = add_caller_identity_for_pstn(Newheaders, FromURI, PSTNgateway),
-    sipauth:check_and_send_auth(Header, Newheaders2, Socket, Fromphone, Phone,
-				{siprequest, send_proxy_request},
-				{Method, Newlocation, Body, []},
-				Method, Classdefs).
+    {_, _, PSTNgateway, _, _} = NewURI,
+    NewHeaders2 = case sipserver:get_env(record_route, true) of
+	true -> siprequest:add_record_route(NewHeaders1);
+        false -> NewHeaders1
+    end,
+    {_, FromURI} = sipheader:from(keylist:fetch("From", Header)),
+    NewHeaders3 = add_caller_identity_for_pstn(Method, NewHeaders2, FromURI, PSTNgateway),
+    NewRequest = {Method, URI, NewHeaders3, Body},
+    relay_request_to_pstn(Socket, NewRequest, NewURI, DstNumber, LogStr).
+    
+relay_request_to_pstn(Socket, {"ACK", URI, Header, Body}, NewURI, DstNumber, LogStr) ->
+    logger:log(normal, "~s -> PSTN ~s (~s)", [LogStr, DstNumber, sipurl:print(URI)]),
+    siprequest:send_proxy_request(Socket, {"ACK", URI, Header, Body}, NewURI, []);
+
+relay_request_to_pstn(Socket, {"CANCEL", URI, Header, Body}, NewURI, DstNumber, LogStr) ->
+    logger:log(normal, "~s -> PSTN ~s (~s)", [LogStr, DstNumber, sipurl:print(URI)]),
+    siprequest:send_proxy_request(Socket, {"CANCEL", URI, Header, Body}, NewURI, []);
+
+relay_request_to_pstn(Socket, Request, NewURI, DstNumber, LogStr) ->
+    {Method, URI, Header, Body} = Request,
+    {_, FromURI} = sipheader:to(keylist:fetch("From", Header)),
+    Classdefs = sipserver:get_env(classdefs, [{"", unknown}]),
+    case sipauth:pstn_call_check_auth(Method, Header, sipurl:print(FromURI), DstNumber, Classdefs) of
+	{true, User, Class} ->
+	    logger:log(debug, "Auth: User ~p is allowed to call dst ~s (class ~s)", [User, DstNumber, Class]),
+	    logger:log(normal, "~s -> PSTN ~s (~s)", [LogStr, DstNumber, sipurl:print(NewURI)]),
+	    siprequest:send_proxy_request(Socket, Request, NewURI, []);
+	{stale, User, Class} ->
+	    logger:log(debug, "Auth: User ~p must authenticate for dst ~s (class ~s)", [User, DstNumber, Class]),
+	    siprequest:send_proxyauth_req(dropack_to(Header), Socket, sipauth:get_challenge(), true);
+	{false, User, Class} ->
+	    logger:log(debug, "Auth: User ~p must authenticate for dst ~s (class ~s)", [User, DstNumber, Class]),
+	    siprequest:send_proxyauth_req(dropack_to(Header), Socket, sipauth:get_challenge(), false);
+	Unknown ->
+	    logger:log(error, "Auth: Unknown result from sipauth:pstn_is_allowed_call() :~n~p", [Unknown]),
+	    throw({siperror, 500, "Server Internal Error"})
+    end.
 
 response(Status, Reason, Header, Body, Socket, FromIP) ->
     LogStr = sipserver:make_logstr({response, Status, Reason, Header, Body}, FromIP),
     logger:log(normal, "Response to ~s: ~p ~s", [LogStr, Status, Reason]),
     siprequest:send_proxy_response(Socket, Status, Reason, Header, Body).
 
-add_caller_identity_for_pstn(Headers, URI, Gateway) ->
-    NewHeaders1 = case sipserver:get_env(remote_party_id, false) of
+add_caller_identity_for_pstn("INVITE", Headers, URI, Gateway) ->
+    case sipserver:get_env(remote_party_id, false) of
 	true ->
-	    case local:get_remote_party_number(local:sipuser(URI), URI, Gateway) of
+	    case local:get_remote_party_number(URI, Gateway) of
 		none ->
 		    Headers;
-		RPI ->
-		    RemotePartyId = RPI ++ ";party=calling",
+		Number ->
+		    {User, _, Host, Port, _} = URI,
+		    RemotePartyId = sipurl:print({Number, none, Host, Port, ["party=calling", "screen=yes", "privacy=off"]}),
 		    logger:log(debug, "Remote-Party-Id: ~s", [RemotePartyId]),
-		    NewHeaders2 = keylist:delete("Remote-Party-Id", Headers),
-		    keylist:set("Remote-Party-Id", [RemotePartyId], NewHeaders2)
+		    NewHeaders1 = keylist:set("Remote-Party-Id", [RemotePartyId], Headers),
+		    logger:log(debug, "P-Preferred-Identity: ~s", ["tel:" ++ Number]),
+		    keylist:set("P-Preferred-Identity", ["tel:" ++ Number], NewHeaders1)
 	    end;
 	_ ->
 	    Headers
-    end.
+    end;
+add_caller_identity_for_pstn(_, Headers, _, _) ->
+    Headers.
+    
+add_caller_identity_for_sip("INVITE", Headers, URI) ->
+    case sipserver:get_env(remote_party_id, false) of
+	true ->
+	    {User, Pass, Host, Port, Parameters} = URI,
+	    case local:get_remote_party_name(User, URI) of
+		none ->
+		    Headers;
+		DisplayName ->
+		    [RPI] = sipheader:contact_print([{DisplayName, URI}]),
+		    RemotePartyId = RPI ++ ";party=calling;screen=yes;privacy=off",
+		    logger:log(debug, "Remote-Party-Id: ~s", [RemotePartyId]),
+		    keylist:set("Remote-Party-Id", [RemotePartyId], Headers)
+	    end;
+	_ ->
+	    Headers
+    end;
+add_caller_identity_for_sip(_, Headers, _) ->
+    Headers.
 
-add_caller_identity_for_sip(Headers, URI, Gateway) ->
-    {User, _, _, _, _} = URI,
-    NewHeaders1 = case sipserver:get_env(remote_party_id, false) of
-	true ->
-	    case local:get_remote_party_name(User, URI, Gateway) of
-		none ->
-		    Headers;
-		RPI ->
-		    RemotePartyId = RPI ++ ";party=calling",
-		    logger:log(debug, "Remote-Party-Id: ~s", [RemotePartyId]),
-		    NewHeaders2 = keylist:delete("Remote-Party-Id", Headers),
-		    keylist:set("Remote-Party-Id", [RemotePartyId], NewHeaders2)
-	    end;
+% Set a To-tag that includes a magic cookie to recognize the ACK of one of our
+% challenges without having to keep state, except if there already is a to-tag
+dropack_to(Header) ->
+    To = keylist:fetch("To", Header),
+    case sipheader:get_tag(To) of
+	none ->
+	    {DistplayName, ToURI} = sipheader:to(To),
+	    MyHostname = siprequest:myhostname(),
+	    NewTo = lists:concat([sipheader:to_print({DistplayName, ToURI}), ";tag=",
+	    			siprequest:generate_branch(), "-", MyHostname, "-drop-ACK"]),
+	    keylist:set("To", [NewTo], Header);
 	_ ->
-	    Headers
+	    Header
     end.
