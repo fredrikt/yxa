8d09d6c15d0f7cb73a98f4a2f82a0c84baa3084a
Fredrik Thulin <ft@it.su.se>
2004-01-29 13:28:08 +0000

    merge with SU_V2_1: error check on startup, generate timestamp in caller instead of in logger process to get more accuracy, log milliseconds.
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@307 e454d3dc-12ff-0310-9b8b-94264392205f

--- logger.erl
+++ logger.erl
@@ -1,5 +1,5 @@
 -module(logger).
--export([start/0, start/1, recvloop/3, log/2, log/3]).
+-export([start/0, start/1, recvloop/3, log/2, log/3, quit/1]).
 
 start() ->
     ApplicationName = case application:get_application() of
@@ -11,51 +11,73 @@ start() ->
     start(sipserver:get_env(logger_logbasename, ApplicationName)).
 
 start(Basename) ->
-    {ok, DebugIoDevice} = file:open(lists:concat([Basename, ".debug"]), [append]),
-    {ok, NormalIoDevice} = file:open(lists:concat([Basename, ".log"]), [append]),
-    {ok, ErrorIoDevice} = file:open(lists:concat([Basename, ".error"]), [append]),
+    DebugIoDevice = safe_open(lists:concat([Basename, ".debug"]), [append]),
+    NormalIoDevice = safe_open(lists:concat([Basename, ".log"]), [append]),
+    ErrorIoDevice = safe_open(lists:concat([Basename, ".error"]), [append]),
     Pid = spawn(logger, recvloop, [DebugIoDevice, NormalIoDevice, ErrorIoDevice]),
     register(logger, Pid).
 
+safe_open(Filename, Args) ->
+    case file:open(Filename, Args) of
+	{ok, FD} ->
+	    FD;
+	{error, E} ->
+	    erlang:fault("Error opening logfile", [Filename, Args])
+    end.
+
 recvloop(DebugIoDevice, NormalIoDevice, ErrorIoDevice) ->
     receive
-	{log, Level, Format, Arguments, Pid} ->
+	{log, Level, TimeStamp, Format, Arguments, Pid} ->
 	    DebugLevel = debug_level(debug),
 	    NormalLevel = debug_level(normal),
 	    ErrorLevel = debug_level(error),
 	    NumLevel = debug_level(Level),
 	    if 
 		NumLevel >= ErrorLevel ->
-		    log_to_stdout(Level, Format, Arguments, Pid),
-		    log_to_device(ErrorIoDevice, Level, Format, Arguments, Pid);
+		    log_to_stdout(Level, TimeStamp, Format, Arguments, Pid),
+		    log_to_device(ErrorIoDevice, Level, TimeStamp, Format, Arguments, Pid);
 		true -> none
 	    end,
 	    if
 		NumLevel >= NormalLevel ->
-		    log_to_device(NormalIoDevice, Level, Format, Arguments, Pid),
-		    log_to_stdout(Level, Format, Arguments, Pid);
+		    log_to_device(NormalIoDevice, Level, TimeStamp, Format, Arguments, Pid),
+		    % Make sure we don't log errors twice to stdout
+		    if
+			NumLevel =< NormalLevel ->
+			    log_to_stdout(Level, TimeStamp, Format, Arguments, Pid);
+			true -> none
+		    end;
 		true -> none
 	    end,
 	    if
 		NumLevel >= DebugLevel ->
-		    log_to_device(DebugIoDevice, Level, Format, Arguments, Pid);
+		    log_to_device(DebugIoDevice, Level, TimeStamp, Format, Arguments, Pid);
 		true -> none
 	    end,
-	    recvloop(DebugIoDevice, NormalIoDevice, ErrorIoDevice)
+	    recvloop(DebugIoDevice, NormalIoDevice, ErrorIoDevice);
+	{quit, Pid} ->
+	     file:close(DebugIoDevice),
+	     file:close(NormalIoDevice),
+	     file:close(ErrorIoDevice),
+	     Pid ! {logger_quit}
     end.
 
 debug_level(debug) -> 10;
 debug_level(normal) -> 20;
 debug_level(error) -> 30.
 
-do_log(Level, Format, Arguments) ->
-    logger ! {log, Level, Format, Arguments, self()},
+do_log(Level, Format, Arguments) when atom(Level), list(Format), list(Arguments) ->
+    {Megasec, Sec, USec} = now(),
+    DateTime = util:sec_to_date(Megasec * 1000000 + Sec),
+    logger ! {log, Level, lists:concat([DateTime, ".", USec]), Format, Arguments, self()},
     ok.
 
-log(Level, Format) ->
-    log(Level, Format, []).
+log(Level, Format) when atom(Level), list(Format) ->
+    log(Level, Format, []);
+log(Foo, Bar) ->
+    log(error, "PROGRAMMING ERROR: Incorrect use of logger:log(), arguments :~n~p ~p", [Foo, Bar]).
 
-log(Level, Format, Arguments) ->
+log(Level, Format, Arguments) when atom(Level), list(Format), list(Arguments) ->
     case catch do_log(Level, Format, Arguments) of
 	ok ->
 	    ok;
@@ -65,9 +87,8 @@ log(Level, Format, Arguments) ->
 	    ok
     end.
 
-log_to_device(IoDevice, Level, Format, Arguments, Pid) ->
-    io:format(IoDevice, "~s ", [util:sec_to_date(util:timestamp())]),
-    io:format(IoDevice, "~p~p:", [Level, Pid]),
+log_to_device(IoDevice, Level, TimeStamp, Format, Arguments, Pid) ->
+    io:format(IoDevice, "~s ~p~p:", [TimeStamp, Level, Pid]),
     case catch io:format(IoDevice, Format, Arguments) of
 	{'EXIT', E} ->
 	    io:format(IoDevice, "LOG FORMATTING ERROR ~p, Format : ~p~n", [E, Format]);
@@ -76,8 +97,8 @@ log_to_device(IoDevice, Level, Format, Arguments, Pid) ->
     end,
     io:format(IoDevice, "~n", []).
 
-log_to_stdout(Level, Format, Arguments, Pid) ->
-    io:format("~s ~p ", [util:sec_to_date(util:timestamp()), Pid]),
+log_to_stdout(Level, TimeStamp, Format, Arguments, Pid) ->
+    io:format("~s ~p ", [TimeStamp, Pid]),
     case catch io:format(Format, Arguments) of
 	{'EXIT', E} ->
 	    io:format("LOG FORMATTING ERROR ~p, Format : ~p~n", [E, Format]);
@@ -85,3 +106,19 @@ log_to_stdout(Level, Format, Arguments, Pid) ->
 	    true
     end,
     io:format("~n", []).
+
+quit(Msg) ->
+    case Msg of
+	none -> true;
+	[] -> true;
+	_ ->
+	    log(normal, Msg)
+    end,
+    logger ! {quit, self()},
+    receive
+	{logger_quit} ->
+	    ok
+    after
+	1000 ->
+	    timeout
+    end.
