fed7c92b1b45bc053d3c0c53b5ff3bcb0caceb85
Fredrik Thulin <ft@it.su.se>
2004-02-19 17:33:09 +0000

    merge with SU_V3_0: use records
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@343 e454d3dc-12ff-0310-9b8b-94264392205f

--- siptimer.erl
+++ siptimer.erl
@@ -1,139 +1,203 @@
 -module(siptimer).
--export([add_timer/6, revive_timer/3, get_timer/2, get_timers_appid_matching/2,
-	 get_timers_appsignal_matching/2, extract_appid/1,
-	 extract_appsignal/1, extract_timeout/1, cancel_timer/2, cancel_all_timers/1,
-	 cancel_timer_seq/2, cancel_timers/2, get_timers_seqs/2, extract_seqno/1,
-	 extract_description/1, extract_starttime/1, 
-	 cancel_timers_with_appid/2, stop_timers/1, debugfriendly/1, timeout2str/1]).
+-export([add_timer/4, revive_timer/3, reset_timers/2, get_timer/2,
+	 get_timers_appsignal_matching/2, cancel_timer/2, cancel_all_timers/1,
+	 cancel_timers/2, cancel_timers_with_appsignal/2, stop_timers/1,
+	 debugfriendly/1, timeout2str/1, extract/2, empty/0]).
 
-add_timer(_, 0, _, _, _, TimerList) ->
+-record(siptimerlist, {list}).
+-record(siptimer, {ref, timer, timeout, description, starttime, appsignal}).
+
+add_timer(0, _, _, TimerList) when record(TimerList, siptimerlist) ->
     TimerList;
-add_timer(TimerSeq, Timeout, Description, AppSignal, AppId, TimerList) ->
-    {ok, NewTimerRef} = timer:send_after(Timeout, {siptimer, TimerSeq, Description}),
+add_timer(Timeout, Description, AppSignal, TimerList) when record(TimerList, siptimerlist) ->
+    Ref = make_ref(),
+    {ok, NewTimerRef} = timer:send_after(Timeout, {siptimer, Ref, Description}),
     TimeoutStr = timeout2str(Timeout),
-    logger:log(debug, "Siptimer: set up timer ~p:~p, ~s seconds", [TimerSeq, Description, TimeoutStr]),
-    lists:append(TimerList, [{TimerSeq, NewTimerRef, Timeout, Description, util:timestamp(), AppSignal, AppId}]).
+    logger:log(debug, "Siptimer: Set up timer ~p:~p, ~s seconds", [Ref, Description, TimeoutStr]),
+    NewSipTimer = #siptimer{ref=Ref, timer=NewTimerRef, timeout=Timeout, description=Description, starttime=util:timestamp(), appsignal=AppSignal},
+    make_siptimerlist(lists:append(TimerList#siptimerlist.list, [NewSipTimer])).
 
-revive_timer(Timer, NewTimeout, TimerList) ->
-    {TimerSeq, OldTimerRef, OldTimeout, Description, OldStartTime, AppSignal, AppId} = Timer,
-    case get_timer(TimerSeq, TimerList) of
+empty() ->
+    make_siptimerlist([]).
+
+reset_timers(ResetList, TimerList) when record(ResetList, siptimerlist), record(TimerList, siptimerlist) ->
+    reset_timers2(ResetList#siptimerlist.list, TimerList).
+
+reset_timers2([], TimerList) ->
+    TimerList;
+reset_timers2([H|T], TimerList) when record(H, siptimer), record(TimerList, siptimerlist) ->
+    Ref = H#siptimer.ref,
+    case get_timer(Ref, TimerList) of
         none ->
-	    logger:log(error, "Siptimer: Can't revive timer ~p:~p gone from list :~n~p~n", [TimerSeq, Description, TimerList]),
+	    Description = H#siptimer.description,
+	    logger:log(error, "Siptimer: Can't reset timer ~p:~p gone from list :~n~p~n", [Ref, Description, TimerList]),
 	    TimerList;
-        _ ->
-	    logger:log(debug, "Siptimer: Reviving timer ~p:~p with new timeout, ~p seconds", [TimerSeq, Description, NewTimeout div 1000]),
-	    {ok, NewTimerRef} = timer:send_after(NewTimeout, {siptimer, TimerSeq, Description}),
-	    lists:keyreplace(TimerSeq, 1, TimerList, {TimerSeq, NewTimerRef, NewTimeout, Description, util:timestamp(), AppSignal, AppId})
+        ThisTimer ->
+	    timer:cancel(ThisTimer#siptimer.timer),
+            Description = ThisTimer#siptimer.description,
+            Timeout = ThisTimer#siptimer.timeout,
+	    OldStarttime = ThisTimer#siptimer.starttime,
+	    SecondsLeft = (OldStarttime + (Timeout div 1000)) - util:timestamp(),
+	    logger:log(debug, "Siptimer: Timer ~p:~p was about to fire in ~p seconds, resetting with new timeout, ~s seconds",
+			[Ref, Description, SecondsLeft, timeout2str(Timeout)]),
+	    {ok, NewTimerRef} = timer:send_after(Timeout, {siptimer, Ref, Description}),
+	    update_timer(ThisTimer#siptimer{timer=NewTimerRef}, TimerList)
+    end.
+
+revive_timer(SipTimer, NewTimeout, TimerList) when record(SipTimer, siptimer), record(TimerList, siptimerlist) ->
+    Ref = SipTimer#siptimer.ref,
+    case get_timer(Ref, TimerList) of
+        none ->
+	    Description = SipTimer#siptimer.description,
+	    logger:log(error, "Siptimer: Can't revive timer ~p:~p gone from list :~n~p~n", [Ref, Description, TimerList]),
+	    TimerList;
+        ThisTimer ->
+            Description = ThisTimer#siptimer.description,
+	    logger:log(debug, "Siptimer: Reviving timer ~p:~p with new timeout, ~s seconds", [Ref, Description, timeout2str(NewTimeout)]),
+	    {ok, NewTimerRef} = timer:send_after(NewTimeout, {siptimer, Ref, Description}),
+	    update_timer(ThisTimer#siptimer{timeout=NewTimeout,timer=NewTimerRef}, TimerList)
     end.
 	
-get_timer(TimerSeq, TimerList) ->
-    case lists:keysearch(TimerSeq, 1, TimerList) of
-	false ->
-	    none;
-	{value, Timer} ->
-	    Timer
-    end.
+update_timer(SipTimer, TList) when record(SipTimer, siptimer), record(TList, siptimerlist) ->
+    Ref = SipTimer#siptimer.ref,
+    make_siptimerlist(update_timer2(Ref, SipTimer, TList#siptimerlist.list, TList)).
+    
+make_siptimerlist([]) ->
+    #siptimerlist{list=[]};
+make_siptimerlist(In) ->
+    #siptimerlist{list=make_siptimerlist2(In, [])}.
 
-get_timers_appid_matching(AppId, []) ->
+make_siptimerlist2([], Res) ->
+    Res;
+make_siptimerlist2([H | T], Res) when record(H, siptimer) ->
+    make_siptimerlist2(T, lists:append(Res, [H]));
+make_siptimerlist2(In, Res) ->
+    erlang:fault("Trying to make siptimerlist of something else than siptimers!", [In, Res]).
+
+update_timer2(Ref, _, [], TList) ->
+    logger:log(error, "Siptimer: Asked to update a timer, but I can't find it", [Ref]),
+    logger:log(error, "Siptimer: Asked to update a timer with ref=~p, but I can't find it in list :~n~p",
+		[Ref, debugfriendly(TList)]),
     [];
-get_timers_appid_matching(AppId, [{TimerSeq, TimerRef, Timeout, Description, StartTime, AppSignal, AppId} | Rest]) ->
-    lists:append([{TimerSeq, TimerRef, Timeout, Description, StartTime, AppSignal, AppId}], get_timers_appid_matching(AppId, Rest));
-get_timers_appid_matching(AppId, [NoMatch | Rest]) ->
-    get_timers_appid_matching(AppId, Rest).
+update_timer2(Ref, NewT, [H | T], TList) when record(H, siptimer), H#siptimer.ref == Ref ->
+    [NewT | T];
+update_timer2(Ref, NewT, [H | T], TList) when record(H, siptimer) ->
+    lists:append([H], update_timer2(Ref, NewT, T, TList)).
 
-get_timers_appsignal_matching(AppSignal, []) ->
+
+get_timer(Ref, TimerList) when record(TimerList, siptimerlist) ->
+    get_timer2(Ref, TimerList#siptimerlist.list).
+
+get_timer2(Ref, []) ->
+    none;
+get_timer2(Ref, [H | T]) when record(H, siptimer), H#siptimer.ref == Ref ->
+    H;
+get_timer2(Ref, [H | T]) when record(H, siptimer) ->
+    get_timer2(Ref, T).
+
+
+get_timers_appsignal_matching(AppSignal, TimerList) when record(TimerList, siptimerlist) ->
+    make_siptimerlist(get_timers_something_matching2(appsignal, AppSignal, TimerList#siptimerlist.list, [])).
+
+get_timers_something_matching2(Key, Value, [], Res) ->
+    Res;
+get_timers_something_matching2(appsignal, Value, [H | T], Res) when record(H, siptimer), H#siptimer.appsignal == Value ->
+    get_timers_something_matching2(appsignal, Value, T, lists:append(Res, [H]));
+get_timers_something_matching2(Key, Value, [H | T], Res) when record(H, siptimer) ->
+    get_timers_something_matching2(Key, Value, T, Res).    
+
+
+extract(Values, SipTimer) when record(SipTimer, siptimer) ->
+    extract(Values, SipTimer, []).
+
+extract([], SipTimer, Res) when record(SipTimer, siptimer) ->
+    Res;
+extract([ref | T], SipTimer, Res) when record(SipTimer, siptimer) ->
+    extract(T, SipTimer, lists:append(Res, [SipTimer#siptimer.ref]));
+extract([timeout | T], SipTimer, Res) when record(SipTimer, siptimer) ->
+    extract(T, SipTimer, lists:append(Res, [SipTimer#siptimer.timeout]));
+extract([description | T], SipTimer, Res) when record(SipTimer, siptimer) ->
+    extract(T, SipTimer, lists:append(Res, [SipTimer#siptimer.description]));
+extract([starttime | T], SipTimer, Res) when record(SipTimer, siptimer) ->
+    extract(T, SipTimer, lists:append(Res, [SipTimer#siptimer.starttime]));
+extract([appsignal | T], SipTimer, Res) when record(SipTimer, siptimer) ->
+    extract(T, SipTimer, lists:append(Res, [SipTimer#siptimer.appsignal])).
+
+cancel_timer(SipTimer, TimerList) when record(SipTimer, siptimer), record(TimerList, siptimerlist) ->
+    Timer = SipTimer#siptimer.timer,
+    Ref = SipTimer#siptimer.ref,
+    Descr = SipTimer#siptimer.description,
+    logger:log(debug, "Siptimer: Cancelling timer ~p:~p", [Ref, Descr]),
+    timer:cancel(Timer),
+    make_siptimerlist(del_ref(Ref, TimerList#siptimerlist.list)).
+
+del_ref(Ref, []) ->
     [];
-get_timers_appsignal_matching(AppSignal, [{TimerSeq, TimerRef, Timeout, Description, StartTime, AppSignal, AppId} | Rest]) ->
-    lists:append([{TimerSeq, TimerRef, Timeout, Description, StartTime, AppSignal, AppId}], get_timers_appsignal_matching(AppSignal, Rest));
-get_timers_appsignal_matching(AppSignal, [NoMatch | Rest]) ->
-    get_timers_appsignal_matching(AppSignal, Rest).
-
-get_timers_seqs([], TimerList) ->
-    [];
-get_timers_seqs([TSeq|Rest], TimerList) ->
-    [get_timer(TSeq, TimerList) | get_timers_seqs(Rest, TimerList)];
-get_timers_seqs([TSeq], TimerList) ->
-    [get_timer(TSeq, TimerList)].
-
-extract_seqno({Seq, _, _, _, _, _, _}) ->
-    Seq.
-extract_timeout({_, _, Timeout, _, _, _, _}) ->
-    Timeout.
-extract_description({_, _, _, Descr, _, _, _}) ->
-    Descr.
-extract_starttime({_, _, _, _, StartTime, _, _}) ->
-    StartTime.
-extract_appsignal({_, _, _, _, _, AppSignal, _}) ->
-    AppSignal.
-extract_appid({_, _, _, _, _, _, AppId}) ->
-    AppId.
-
-cancel_timer_seq(TimerSeq, TimerList) ->
-    case get_timer(TimerSeq, TimerList) of
-	none ->
-	    false;
-	{TimerSeq, TRef, _, Descr, _, _, _} ->
-	    logger:log(debug, "Siptimer: Cancelling timer ~p:~p", [TimerSeq, Descr]),
-	    timer:cancel(TRef)
-    end,
-    lists:keydelete(TimerSeq, 1, TimerList).
-
-cancel_timer([], TimerList) ->
-    logger:log(error, "Siptimer: cancel_timer() called with empty Timer!"),
-    false;
-cancel_timer(Timer, []) ->
-    logger:log(error, "Siptimer: cancel_timer() called with empty TimerList!"),
-    false;
-cancel_timer(Timer, TimerList) when list(TimerList) ->
-    {TimerSeq, TRef, _, Descr, _, _, _} = Timer,
-    logger:log(debug, "Siptimer: Cancelling timer ~p:~p", [TimerSeq, Descr]),
-    timer:cancel(TRef),
-    lists:keydelete(TimerSeq, 1, TimerList).
+del_ref(Ref, [H | T]) when record(H, siptimer), H#siptimer.ref == Ref ->
+    del_ref(Ref, T);
+del_ref(Ref, [H | T]) when record(H, siptimer) ->
+    [H | del_ref(Ref, T)].
 
 stop_timers([]) ->
     logger:log(debug, "Siptimer: Asked to stop timers but none were supplied"),
-    [];
-stop_timers([Timer | Rest]) ->
-    {TimerSeq, TRef, _, Descr, _, _, _} = Timer,
-    logger:log(debug, "Siptimer: Stopping timer ~p:~p", [TimerSeq, Descr]),
-    timer:cancel(TRef),
-    stop_timers(Rest).
+    error;
+stop_timers(TimerList) when record(TimerList, siptimerlist) ->
+    stop_timers2(TimerList#siptimerlist.list),
+    ok.
 
-cancel_timers(TimerList, []) ->
-    logger:log(debug, "Siptimer: Asked to cancel timers but none were supplied"),
-    [];
-cancel_timers(TimerList, [Timer]) ->
-    cancel_timer(Timer, TimerList);
-cancel_timers(TimerList, [Timer | Rest]) ->
-    NewTList = cancel_timer(Timer, TimerList),
-    cancel_timers(NewTList, Rest).
+stop_timers2([SipTimer | Rest]) ->
+    Timer = SipTimer#siptimer.timer,
+    Ref = SipTimer#siptimer.ref,
+    Descr = SipTimer#siptimer.description,
+    logger:log(debug, "Siptimer: Stopping timer ~p:~p", [Ref, Descr]),
+    timer:cancel(Timer),
+    stop_timers2(Rest).
+
+cancel_timers(CancelTimers, TimerList) when record(CancelTimers, siptimerlist), record(TimerList, siptimerlist) ->
+    cancel_timers(CancelTimers#siptimerlist.list, TimerList);
+
+cancel_timers([], TimerList) when record(TimerList, siptimerlist) ->
+    TimerList;
+cancel_timers([H | T], TimerList) when record(H, siptimer), record(TimerList, siptimerlist) ->
+    NewTList = cancel_timer(H, TimerList),
+    cancel_timers(T, NewTList).
 
 cancel_all_timers([]) ->
     logger:log(debug, "Siptimer: Asked to cancel all timers but there are no timers to cancel"),
-    [];
-cancel_all_timers(TimerList) ->
+    empty();
+cancel_all_timers(TimerList) when record(TimerList, siptimerlist) ->
     logger:log(debug, "Siptimer: Cancelling all timers :"),
-    lists:map(fun (Timer) ->
-    		      {TSeq, TRef, _, Descr, _, _, _} = Timer,
-		      logger:log(debug, "Siptimer:   ~p:~p", [TSeq, Descr]),
-		      timer:cancel(TRef)
-		end, TimerList),
-    [].
+    lists:map(fun (SipTimer) ->
+			Timer = SipTimer#siptimer.timer,
+			Ref = SipTimer#siptimer.ref,
+			Descr = SipTimer#siptimer.description,
+			logger:log(debug, "Siptimer:    ~p:~p", [Ref, Descr]),
+			timer:cancel(Timer)
+		end, TimerList#siptimerlist.list),
+    empty().
 
-cancel_timers_with_appid(AppId, TimerList) when list(TimerList) ->
-    case get_timers_appid_matching(AppId, TimerList) of
-	[] ->
-	    logger:log(debug, "Siptimer: No timers with AppId ~p found in TimerList :~n~p", [AppId, debugfriendly(TimerList)]),
+cancel_timers_with_appsignal(AppSignal, TimerList) when record(TimerList, siptimerlist) ->
+    case get_timers_appsignal_matching(AppSignal, TimerList) of
+	Foo when record(Foo, siptimerlist), Foo#siptimerlist.list == [] ->
+	%    logger:log(debug, "Siptimer: No timers with AppSignal ~p found in TimerList :~n~p",
+	%		[AppSignal, debugfriendly(TimerList)]),
 	    TimerList;
-    CancelTimers ->
-	    logger:log(debug, "Siptimer: Cancelling all timers with AppId ~p :", [AppId]),
-	    cancel_timers(TimerList, CancelTimers)
+	CancelTimers when record(CancelTimers, siptimerlist) ->
+	    logger:log(debug, "Siptimer: Cancelling all timers with AppSignal ~p :", [AppSignal]),
+	    cancel_timers(CancelTimers, TimerList)
     end.
 
+debugfriendly(TimerList) when record(TimerList, siptimerlist) ->
+    debugfriendly(TimerList#siptimerlist.list);
 debugfriendly([]) ->
     [];
-debugfriendly([{TSeq, TRef, Timeout, Descr, Starttime, AppSignal, AppId} | Rest]) ->
-    lists:append([lists:concat(["timer seq ", TSeq, ": ", Descr])], debugfriendly(Rest)).
+debugfriendly([H | Rest]) when record(H, siptimer) ->
+    RefStr = io_lib:format("~p", [H#siptimer.ref]),
+    Descr = H#siptimer.description,
+    SignalStr = io_lib:format("~p", [H#siptimer.appsignal]),
+    Str = lists:concat([RefStr, ":", Descr, " -> ", SignalStr]),
+    lists:append([lists:flatten(Str)], debugfriendly(Rest)).
+
 
 timeout2str(500) ->
     "0.5";
--- targetlist.erl
+++ targetlist.erl
@@ -1,9 +1,11 @@
 -module(targetlist).
--export([add_target/6, debugfriendly/1,
-	 get_target/2, get_target_by_header/2, get_targets_in_state/2, get_responses/1,
-	 extract_branch/1, extract_initialrequest/1, extract_pid/1, extract_state/1, extract_endresult/1,
-         set_pid/2, set_state/2, set_endresult/2,
+-export([add/7, empty/0, list_length/1, debugfriendly/1,
+	 get_using_branch/2, get_targets_in_state/2, get_responses/1,
+	 extract/2, set_pid/2, set_state/2, set_endresult/2, set_dstlist/2,
          update_target/2]).
+
+-record(target, {ref, branch, request, pid, state, timeout, endresult, dstlist}).
+-record(targetlist, {list}).
 
 % This module is used by sipproxy to handle a list of actions we are working
 % with.
@@ -15,96 +17,115 @@
 %       InitialRequest = {Method, URI, Header, Body}
 %	EndResult = {Status, Reason, Header, Body}
 
-add_target(Branch, InitialRequest, Pid, State, EndResult, TargetList) ->
-    case get_target(Branch, TargetList) of
+add(Branch, Request, Pid, State, Timeout, DstList, TargetList) when record(TargetList, targetlist) ->
+    case get_using_branch(Branch, TargetList) of
 	none ->
-	    lists:append(TargetList, [{Branch, InitialRequest, Pid, State, EndResult}]);	
+	    NewTarget = #target{ref=make_ref(), branch=Branch, request=Request, pid=Pid, state=State,
+		   		endresult=none, dstlist=DstList, timeout=Timeout},
+	    #targetlist{list=lists:append(TargetList#targetlist.list, [NewTarget])};
 	_ ->
-	    logger:log(error, "Targetlist: Asked to add request with duplicate Branch ~p to list :~n~p",
-	    	       [Branch, TargetList]),
+	    logger:log(error, "targetlist: Asked to add target with duplicate branch ~p to list :~n~p",
+	    	       [branch, debugfriendly(TargetList)]),
 	    TargetList
     end.
 
+empty() ->
+    #targetlist{list=[]}.
+
+list_length(TargetList) when record(TargetList, targetlist) ->
+    length(TargetList#targetlist.list).
+
+update_target(Target, TargetList) when record(Target, target), record(TargetList, targetlist) ->
+    Ref = Target#target.ref,
+    #targetlist{list=update_target(Ref, Target, TargetList#targetlist.list, TargetList)}.
+
+update_target(Ref, _, [], TargetList) ->
+    logger:log(error, "Targetlist: Asked to update a target, but I can't find it", [Ref]),
+    logger:log(error, "Targetlist: Asked to update a target with ref=~p, but I can't find it in list :~n~p",
+		[Ref, debugfriendly(TargetList)]),
+    [];
+update_target(Ref, NewT, [H | T], TargetList) when record(H, target), H#target.ref == Ref ->
+    [NewT | T];
+update_target(Ref, NewT, [H | T], TargetList) when record(H, target) ->
+    lists:append([H], update_target(Ref, NewT, T, TargetList)).
+
+debugfriendly(TargetList) when record(TargetList, targetlist) ->
+    debugfriendly(TargetList#targetlist.list);
 debugfriendly([]) ->
     [];
-debugfriendly([{Branch, Request, Pid, State, EndResult} | Rest]) ->
-    {Method, URI, _, _} = Request,
-    RespStr = case EndResult of
+debugfriendly([H | T]) when record(H, target) ->
+    {Method, URI, _, _} = H#target.request,
+    RespStr = case H#target.endresult of
 	none -> "no response";
         cancelled -> "cancelled";
 	{Status, Reason, _, _} ->
 	    lists:concat(["response=", Status, " ", Reason])
     end,
-    Str = lists:concat(["branch=", Branch, ", request=", Method, " ", sipurl:print(URI), ", ", RespStr, ", state=" , State]),
-    lists:append([{Pid, Str}], debugfriendly(Rest));
-debugfriendly([Unknown | Rest]) ->
-    lists:append([{"UNKNOWN: ", Unknown}], debugfriendly(Rest)).
+    Str = lists:concat(["pid=", pid_to_list(H#target.pid), "branch=", H#target.branch, ", request=", Method, " ",
+		sipurl:print(URI), ", ", RespStr, ", state=" , H#target.state]),
+    lists:append([lists:flatten(Str)], debugfriendly(T)).
+get_using_branch(Branch, TargetList) when record(TargetList, targetlist) ->
+    get_using_branch(Branch, TargetList#targetlist.list);
 
-get_target(Branch, TargetList) when list(TargetList) ->
-    case lists:keysearch(Branch, 1, TargetList) of
-	false ->
-	    none;
-	{value, Target} ->
-	    Target
-    end.
+get_using_branch(Branch, []) ->
+    none;
+get_using_branch(Branch, [H | T]) when record(H, target), H#target.branch == Branch ->
+    H;
+get_using_branch(Branch, [H | T]) when record(H, target) ->
+    get_using_branch(Branch, T).
 
-get_target_by_header(Header, TargetList) ->
-    case sipproxy:get_branch(Header) of
-        none ->
-            logger:log(error, "Targetlist: Could not get branch from header, Vias :~n~p",
-            		[sipheader:via(keylist:fetch("Via", Header))]),
-            none;
-        Branch ->
-	    get_target(Branch, TargetList)
-    end.
+
+get_targets_in_state(State, TargetList) when record(TargetList, targetlist) ->
+    get_targets_in_state(State, TargetList#targetlist.list);
 
 get_targets_in_state(State, []) ->
     [];
-get_targets_in_state(State, [{Branch, InitialRequest, Pid, State, EndResult} | Rest]) ->
-    [{Branch, InitialRequest, Pid, State, EndResult} | get_targets_in_state(State, Rest)];
-get_targets_in_state(State, [_ | Rest]) ->
-    get_targets_in_state(State, Rest).
-
-get_responses([]) ->
-    [];
-get_responses([{_, _, _, _, {Status, Reason, Header, Body}} | Rest]) when integer(Status) ->
-    lists:append([{Status, Reason, Header, Body}], get_responses(Rest));
-get_responses([{_, _, _, _, Response} | Rest]) ->
-    get_responses(Rest).
-
-extract_branch({Branch, InitialRequest, Pid, State, EndResult}) ->
-    Branch.
-extract_initialrequest({Branch, InitialRequest, Pid, State, EndResult}) ->
-    InitialRequest.
-extract_pid({Branch, InitialRequest, Pid, State, EndResult}) ->
-    Pid.
-extract_state({Branch, InitialRequest, Pid, State, EndResult}) ->
-    State.
-extract_endresult({Branch, InitialRequest, Pid, State, EndResult}) ->
-    EndResult.
+get_targets_in_state(State, [H | T]) when record(H, target), H#target.state == State ->
+    lists:append(get_targets_in_state(State, T), [H]);
+get_targets_in_state(State, [H | T]) when record(H, target) ->
+    get_targets_in_state(State, T).
 
 
-set_pid(Target, NewPid) ->
-    {Branch, InitialRequest, Pid, State, EndResult} = Target,
-    {Branch, InitialRequest, NewPid, State, EndResult}.
-set_state(Target, NewState) ->
-    {Branch, InitialRequest, Pid, State, EndResult} = Target,
-    {Branch, InitialRequest, Pid, NewState, EndResult}.
-set_endresult(Target, NewEndResult) ->
-    {Branch, InitialRequest, Pid, State, EndResult} = Target,
-    {Branch, InitialRequest, Pid, State, NewEndResult}.
+get_responses(TargetList) when record(TargetList, targetlist) ->
+    get_responses2(TargetList#targetlist.list, []).
 
-
-update_target(NewTarget, TargetList) ->
-    {Branch, _, _, _, _} = NewTarget,
-    case get_target(Branch, TargetList) of
-	none ->
-	    logger:log(error, "Targetlist: Can't modify target with unknown branch ~p, targetlist :~n~p", [Branch, TargetList]),
-	    TargetList;
-	NewTarget ->
-	    TargetList;	
-	OldTarget ->
-	    %logger:log(debug, "Targetlist: Updating target :~nold: ~p~nnew: ~p", [OldTarget, NewTarget]),
-	    lists:keyreplace(Branch, 1, TargetList, NewTarget)
+get_responses2([], Res) ->
+    Res;
+get_responses2([H | T], Res) when record(H, target) ->
+    case H#target.endresult of
+	{Status, Reason, Header, Body} ->
+	    get_responses2(T, lists:append(Res, [H#target.endresult]));
+	_ ->
+	    get_responses2(T, Res)
     end.
+
+
+extract(Values, Target) when record(Target, target) ->
+    extract(Values, Target, []).
+
+extract([], Target, Res) when record(Target, target) ->
+    Res;
+extract([pid | T], Target, Res) when record(Target, target) ->
+    extract(T, Target, lists:append(Res, [Target#target.pid]));
+extract([branch | T], Target, Res) when record(Target, target) ->
+    extract(T, Target, lists:append(Res, [Target#target.branch]));
+extract([request | T], Target, Res) when record(Target, target) ->
+    extract(T, Target, lists:append(Res, [Target#target.request]));
+extract([state | T], Target, Res) when record(Target, target) ->
+    extract(T, Target, lists:append(Res, [Target#target.state]));
+extract([timeout | T], Target, Res) when record(Target, target) ->
+    extract(T, Target, lists:append(Res, [Target#target.timeout]));
+extract([dstlist | T], Target, Res) when record(Target, target) ->
+    extract(T, Target, lists:append(Res, [Target#target.dstlist]));
+extract([endresult | T], Target, Res) when record(Target, target) ->
+    extract(T, Target, lists:append(Res, [Target#target.endresult])).
+
+set_pid(Target, Value) when record(Target, target) ->
+    Target#target{pid=Value}.
+set_state(Target, Value) when record(Target, target) ->
+    Target#target{state=Value}.
+set_endresult(Target, Value) when record(Target, target) ->
+    Target#target{endresult=Value}.
+set_dstlist(Target, Value) when record(Target, target) ->
+    Target#target{dstlist=Value}.
 
