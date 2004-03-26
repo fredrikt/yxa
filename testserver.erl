38e0928acf44d11ae23652fa8fa13bbcba87aeb4
Fredrik Thulin <ft@it.su.se>
2004-03-26 13:40:22 +0000

    update to use transaction layer and OTP startup functions
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@390 e454d3dc-12ff-0310-9b8b-94264392205f

--- testserver.erl
+++ testserver.erl
@@ -1,128 +1,79 @@
 -module(testserver).
--export([start/2, list_calls_in_database/0]).
-
-start(normal, Args) ->
-    Pid = spawn(sipserver, start, [fun init/0, fun request/6,
-				   fun response/6, none]),
-    {ok, Pid}.
+-export([init/0, request/6, response/6]).
 
 init() ->
-    timer:apply_interval(10000, ?MODULE, list_calls_in_database, []),
-    database_call:create_call().
+    database_call:create([node()]),
+    [[fun request/6, fun response/6], none, stateful, {append, []}].
 
 localhostname(Hostname) ->
     util:casegrep(Hostname, sipserver:get_env(myhostnames)).
 
 request(Method, URL, Header, Body, Socket, FromIP) ->
-    LogStr = sipserver:make_logstr({request, Method, URL, Header, Body}, FromIP),
+    Request = {Method, URL, Header, Body},
+    THandler = transactionlayer:get_handler_for_request(Request),
+    LogTag = get_branch_from_handler(THandler),
     case Method of
         "REGISTER" ->
-            process_request(Method, URL, Header, Body, Socket, LogStr);
+            process_request(Method, URL, Header, Body, Socket, LogTag);
 	"INVITE" ->
-	    packet_check_ok(Header),
-	    process_request(Method, URL, Header, Body, Socket, LogStr);
+	    packet_check_ok(Header, LogTag),
+	    process_request(Method, URL, Header, Body, Socket, LogTag);
 	"ACK" ->
-	    process_request(Method, URL, Header, Body, Socket, LogStr);
+	    process_request(Method, URL, Header, Body, Socket, LogTag);
 	"CANCEL" ->
-	    process_request(Method, URL, Header, Body, Socket, LogStr);
+	    process_request(Method, URL, Header, Body, Socket, LogTag);
 	"BYE" ->
-	    process_request(Method, URL, Header, Body, Socket, LogStr);
+	    process_request(Method, URL, Header, Body, Socket, LogTag);
 	_ ->
-	    logger:log(normal, "~s -- NOT IMPLEMENTED", [LogStr]),
-	    siprequest:send_result(Header, Socket, "", 501, "Not Implemented")
+	    logger:log(normal, "~s -- NOT IMPLEMENTED", [LogTag]),
+	    transactionlayer:send_response_handler(THandler, 501, "Not Implemented")
     end.
 
-process_request("REGISTER", URI, Header, Body, Socket, LogStr) ->
+process_request("REGISTER", URI, Header, Body, Socket, LogTag) ->
     {User, Pass, Host, Port, Parameters} = URI,
+    Request = {"REGISTER", URI, Header, Body},
     case localhostname(Host) of
 	true ->
-	    % delete any present Record-Route header (RFC3261, #10.3)
+	    %% delete any present Record-Route header (RFC3261, #10.3)
 	    NewHeader = keylist:delete("Record-Route", Header),
 	    Contacts = sipheader:contact(keylist:fetch("Contact", NewHeader)),
 	    logger:log(debug, "Register: Contact(s) ~p", [sipheader:contact_print(Contacts)]),
-	    siprequest:send_result(NewHeader, Socket, "", 200, "OK",
-	    			   [{"Expires", ["0"]},
-				    {"Contacts", sipheader:contact_print(Contacts)}
-				   ]
-				  );
+	    transactionlayer:send_response_request(Request, 200, "OK",
+						   [{"Expires", ["0"]},
+						    {"Contacts", sipheader:contact_print(Contacts)}]
+						  );
 	_ ->
-	    logger:log(debug, "REGISTER for non-homedomain ~p", [Host]),
-	    siprequest:send_result(Header, Socket, "", 501, "Not Implemented")
+	    logger:log(normal, "~s: REGISTER for non-homedomain ~p", [LogTag, Host]),
+	    transactionlayer:send_response_request(Request, 501, "Not Implemented")
     end;
 
-process_request("INVITE", URI, Header, Body, Socket, LogStr) ->
+process_request("INVITE", URI, Header, Body, Socket, LogTag) ->
     siprequest:send_result(Header, Socket, "", 100, "Trying"),
-    [CallID] = keylist:fetch("Call-ID", Header),
-    case database_call:insert_call_unique(CallID, track, Header, self()) of
-	{atomic, ok} ->
-	    logger:log(debug, "State: Created call ~p", [CallID]),
-	    case get_user(URI) of
-		{404, Text} ->
-		    logger:log(normal, "~s -> -- NO SUCH USER -- 404 ~p",
-		    	       [LogStr, Text]),
-		    siprequest:send_result(Header, Socket, "", 404, Text);
-		{Code, Text} ->
-		    logger:log(normal, "~s -> ~p ~s",
-		    	       [LogStr, Code, Text]),
-		    siprequest:send_result(Header, Socket, "", Code, Text)
-	    end;
-	{aborted, key_exists} ->
-	    logger:log(debug, "State: Received another INVITE for call ~p - ignoring", [CallID]),
-	    true
+    Request = {"INVITE", URI, Header, Body},
+    case get_user(URI) of
+	{404, Reason} ->
+	    logger:log(normal, "~s: Testserver classic response: '404 ~p'", [LogTag, Reason]),
+	    transactionlayer:send_response_request(Request, 404, Reason);
+	{Status, Reason} ->
+	    logger:log(normal, "~s: Testserver response: '~p ~s'", [LogTag, Status, Reason]),
+	    transactionlayer:send_response_request(Request, Status, Reason)
     end;
 
-process_request("ACK", URI, Header, Body, Socket, LogStr) ->
-    [CallID] = keylist:fetch("Call-ID", Header),
-    case database_call:get_call(CallID) of
-	{atomic, [{track, Origheaders, Pid}]} ->
-	    case get_user(URI) of
-		{404, Text} ->
-		    logger:log(normal, "ACK ~s -- NO SUCH USER -- 404 ~s",
-    		               [sipurl:print(URI), Text]),
-		    siprequest:send_result(Header, Socket, "", 404, Text);
-		{_, _} ->
-		    logger:log(debug, "ACK ~s - dropping", [sipurl:print(URI)])
-	    end;	
-	{atomic, []} ->
-	    logger:log(normal, "Call ~p not found", [CallID]),
-	    siprequest:send_result(Header, Socket, "", 481, "Call/Transaction Does Not Exist")
-    end;
+process_request(Method, URI, Header, Body, Socket, LogTag) ->
+    logger:log(normal, "~s: testserver: ~s ~s dropped", [LogTag, Method, sipurl:print(URI)]),
+    true.
 
-process_request(Method, URI, Header, Body, Socket, LogStr) ->
-    [CallID] = keylist:fetch("Call-ID", Header),
-    case database_call:get_call(CallID) of
-	{atomic, [{track, Origheaders, Pid}]} ->
-	    case get_user(URI) of
-		{404, Text} ->
-		    logger:log(normal, "~s ~s -- NO SUCH USER -- 404 ~s",
-    		               [Method, sipurl:print(URI), Text]),
-		    siprequest:send_result(Header, Socket, "", 404, "Not Found");
-		{_, _} ->
-		    case Method of
-			"CANCEL" ->
-			    logger:log(debug, "~s ~s - DELETING call ~s", [Method, sipurl:print(URI), CallID]);
-			"BYE" ->
-			    logger:log(debug, "~s ~s - DELETING call ~s", [Method, sipurl:print(URI), CallID]);
-			_ ->
-			    logger:log(debug, "~s ~s - dropping", [Method, sipurl:print(URI)])
-		    end
-	    end;
-	{atomic, []} ->
-	    logger:log(normal, "Call ~p not found", [CallID]),
-	    siprequest:send_result(Header, Socket, "", 481, "Call/Transaction Does Not Exist")
-    end.
-    
 get_user(URI) ->
     Key = sipurl:print(URI),
     Res = regexp_locate_user(Key, sipserver:get_env(user_db, "")),
     logger:log(debug, "Locate user: ~s -> ~p", [Key, Res]),
     case Res of
         nomatch ->
-           {404, "Not Found"};
+	    {404, "Not Found"};
         Res ->
-           Res
+	    Res
     end.
-    
+
 regexp_locate_user(Input, [{Regexp, Code, Text} | Rest]) ->
     case regexp:match(Input, Regexp) of
 	{match, _, _} ->
@@ -133,44 +84,31 @@ regexp_locate_user(Input, [{Regexp, Code, Text} | Rest]) ->
 	    logger:log(normal, "Error in regexp ~p: ~p", [Regexp, Error]),
 	    []
     end.
-    
+
 response(Status, Reason, Header, Body, Socket, FromIP) ->
     logger:log(normal, "~p ~p - dropping", [Status, Reason]),
     true.
 
-list_calls_in_database() ->
-    Res = database_call:list_calls(),
-    case Res of
-    	{atomic, []} ->
-	    logger:log(debug, "No calls in database");
-	{atomic, Calls} ->
-	    logger:log(debug, "Calls in database:"),
-	    print_calls(Calls);
-	{error, What} ->
-	    logger:log(normal, "FREDRIK 2"),
-	    logger:log(debug, "Could not list calls in database (error: ~p)", [What]);
-	Res ->
-	    logger:log(normal, "FREDRIK 1"),
-	    logger:log(debug, "Could not list calls in database (~p)")
-    end.
+packet_check_ok(Header, LogTag) ->
+    check_no_unsupported_extension(Header, LogTag).
 
-print_calls([]) ->
-    [];
-print_calls([{Type, CallID, Action, _, _}]) ->
-    logger:log(debug, "  ~p ~p ~p", [Type, CallID, Action]);
-print_calls([{Type, CallID, Action, _, _} | Rest]) ->
-    logger:log(debug, "  ~p ~p ~p", [Type, CallID, Action]),
-    print_calls(Rest).
-
-packet_check_ok(Header) ->
-    check_no_unsupported_extension(Header).
-
-check_no_unsupported_extension(Header) ->
+check_no_unsupported_extension(Header, LogTag) ->
     Require = keylist:fetch("Require", Header),
     case Require of
 	[] ->
 	    true;
 	_ ->
-	    logger:log(normal, "UAS Request check: The client requires unsupported extension(s) ~p", [Require]),
+	    logger:log(normal, "~s: UAS Request check: The client requires unsupported extension(s) ~p",
+		       [LogTag, Require]),
 	    throw({siperror, 420, "Bad Extension", [{"Unsupported", Require}]})
+    end.
+
+get_branch_from_handler(TH) ->
+    CallBranch = transactionlayer:get_branch_from_handler(TH),
+    case string:rstr(CallBranch, "-UAS") of
+	0 ->
+	    CallBranch;
+	Index when integer(Index) ->
+            BranchBase = string:substr(CallBranch, 1, Index - 1),
+	    BranchBase
     end.
