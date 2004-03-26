ea6bd6edb18193c4be9500bb161ea8a4b86d0f90
Fredrik Thulin <ft@it.su.se>
2004-03-26 13:33:05 +0000

    turned into a OTP supervisor
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@384 e454d3dc-12ff-0310-9b8b-94264392205f

--- sipsocket.erl
+++ sipsocket.erl
@@ -1,27 +1,76 @@
+%%%-------------------------------------------------------------------
+%%% File    : sipsocket.erl
+%%% Author  : Fredrik Thulin <ft@it.su.se>
+%%% Description : Transaction layer processes supervisor.
+%%%
+%%% Created : 21 Mar 2004 by Fredrik Thulin <ft@it.su.se>
+%%%-------------------------------------------------------------------
 -module(sipsocket).
--export([start/1, check_alive/2, send/4, is_reliable_transport/1, get_socket/3,
-	 sipproto2str/1, via2sipsocketprotocol/1, sipproto2viastr/1,
-	 is_good_socket/1]).
+
+-behaviour(supervisor).
+%%--------------------------------------------------------------------
+%% Include files
+%%--------------------------------------------------------------------
 
 -include("sipsocket.hrl").
 
-start(Port) when integer(Port) ->
-    UDP = sipserver:safe_spawn(sipsocket_udp, start, [Port]),
-    TCP = sipserver:safe_spawn(sipsocket_tcp, start, [Port]),
-    [{sipsocket_udp, UDP}, {sipsocket_tcp, TCP}].
+%%--------------------------------------------------------------------
+%% External exports
+%%--------------------------------------------------------------------
+-export([start_link/1]).
 
-check_alive(_, []) ->
-    ok;
-check_alive(Port, [{Module, Pid} | T]) ->
-    case util:safe_is_process_alive(Pid) of
-	{false, Pid} ->
-	    logger:log(error, "Sipsocket: Module ~p pid ~p not alive, attempting restart.",
-			[Module, Pid]),
-	    apply(Module, start, [Port]);
-	{true, Pid} ->
-	    true
-    end,
-    check_alive(Port, T).
+-export([send/4, is_reliable_transport/1, get_socket/3,
+	 sipproto2str/1, via2sipsocketprotocol/1, sipproto2viastr/1,
+	 is_good_socket/1]).
+
+%%--------------------------------------------------------------------
+%% Internal exports
+%%--------------------------------------------------------------------
+-export([init/1]).
+
+%%--------------------------------------------------------------------
+%% Macros
+%%--------------------------------------------------------------------
+-define(SERVER, ?MODULE).
+
+%%--------------------------------------------------------------------
+%% Records
+%%--------------------------------------------------------------------
+
+%%====================================================================
+%% External functions
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: start_link/0
+%% Description: Starts the supervisor
+%%--------------------------------------------------------------------
+start_link(Port) ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, [Port]).
+
+%%====================================================================
+%% Server functions
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Func: init/1
+%% Returns: {ok,  {SupFlags,  [ChildSpec]}} |
+%%          ignore                          |
+%%          {error, Reason}   
+%%--------------------------------------------------------------------
+init([Port]) ->
+    logger:log(debug, "Transport layer supervisor started"),
+    UDP = {sipsocket_udp, {sipsocket_udp, start_link, [Port]},
+	   permanent, 2000, worker, [sipsocket_udp]},
+    TCP = {tcp_dispatcher, {tcp_dispatcher, start_link, [Port]},
+	   permanent, 2000, worker, [tcp_dispatcher]},
+    {ok,{{one_for_one,5,60}, [UDP, TCP]}}.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+
+%%====================================================================
+%% Interface functions
+%%====================================================================
 
 send(Socket, SendToHost, PortInt, Message) when record(Socket, sipsocket) ->
     apply(Socket#sipsocket.module, send, [Socket, SendToHost, PortInt, Message]).
