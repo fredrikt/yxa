eb491a704ba0c87a5515f61645c74fdbb5ac8ddf
Fredrik Thulin <ft@it.su.se>
2004-04-05 09:01:12 +0000

    changes to support multiple protocols
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@435 e454d3dc-12ff-0310-9b8b-94264392205f

--- sipsocket_tcp.erl
+++ sipsocket_tcp.erl
@@ -1,30 +1,57 @@
 -module(sipsocket_tcp).
--export([start/1, send/4, is_reliable_transport/1, get_socket/2]).
+-export([start/1, send/5, is_reliable_transport/1, get_socket/3]).
 
 -include("socketlist.hrl").
 -include("sipsocket.hrl").
 
 start(Port) when integer(Port) ->
     tcp_dispatcher:start_link(Port).
-    
-send(SipSocket, SendToHost, PortInt, Message) when record(SipSocket, sipsocket) ->
+
+send(SipSocket, Proto, Host, Port, Message) when record(SipSocket, sipsocket), integer(Port), SipSocket#sipsocket.proto /= Proto ->
+    {error, "Protocol mismatch"};
+send(SipSocket, Proto, Host, Port, Message) when record(SipSocket, sipsocket), integer(Port) ->
     SPid = SipSocket#sipsocket.pid,
-    case catch gen_server:call(SPid, {send, {SendToHost, PortInt, Message}}, 1500) of
+    Timeout = get_timeout(SipSocket#sipsocket.proto),
+    case catch gen_server:call(SPid, {send, {Host, Port, Message}}, Timeout) of
 	{send_result, Res} ->
 	    Res;
 	{'EXIT', Reason} ->
 	    {error, Reason}
     end.
 
-get_socket(Host, Port) when list(Host), integer(Port) ->
-    case catch gen_server:call(tcp_dispatcher, {get_socket, Host, Port}, 1500) of
-	{ok, Socket} ->
-	    Socket;
+get_socket(Proto, Host, Port) when atom(Proto), list(Host), integer(Port), Proto == tls; Proto == tls6 ->
+    case sipserver:get_env(enable_experimental_tls, false) of
+	true ->
+	    Timeout = get_timeout(Proto),
+	    case catch gen_server:call(tcp_dispatcher, {get_socket, Proto, Host, Port}, Timeout) of
+		{error, E} ->
+		    {error, E};
+		{ok, Socket} ->
+		    Socket;
+		{'EXIT', Reason} ->
+		    {error, Reason}
+		end;
+	_ ->
+	    {error, "TLS (still experimental) is not enabled"}
+    end;
+
+get_socket(Proto, Host, Port) when atom(Proto), list(Host), integer(Port) ->
+    Timeout = get_timeout(Proto),
+    case catch gen_server:call(tcp_dispatcher, {get_socket, Proto, Host, Port}, Timeout) of
 	{error, E} ->
 	    {error, E};
+	{ok, Socket} ->
+	    Socket;
 	{'EXIT', Reason} ->
             {error, Reason}
     end.
 
-is_reliable_transport(_) ->
-    true.
+is_reliable_transport(_) -> true.
+
+%% Internal functions
+
+get_timeout(tcp) -> 1500;
+get_timeout(tcp6) -> 1500;
+get_timeout(tls) -> 5000;
+get_timeout(tls6) -> 5000.
+
