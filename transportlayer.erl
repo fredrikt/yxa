183644d1d74c75d0329270d8fab6e3a57abf1e1d
Fredrik Thulin <ft@it.su.se>
2004-04-05 09:06:21 +0000

    add compability handling of new record for Request
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@440 e454d3dc-12ff-0310-9b8b-94264392205f

--- transportlayer.erl
+++ transportlayer.erl
@@ -1,16 +1,22 @@
 -module(transportlayer).
--export([start/1, send_proxy_request/4, send_proxy_response/2, send_result/5,
+-export([start/0, send_proxy_request/4, send_proxy_response/2, send_result/5,
 	 send_result/6]).
 
-start(Port) when integer(Port) ->
-    sipsocket:start_link(Port).    
+-include("siprecords.hrl").
+
+start() ->
+    sipsocket:start_link().
 
 send_proxy_response(Socket, Response) ->
     {Status, Reason, Header, Body} = Response,
     siprequest:send_proxy_response(Socket, Status, Reason, Header, Body).
 
-send_proxy_request(Socket, OrigRequest, DstURI, Parameters) ->
-    siprequest:send_proxy_request(Socket, OrigRequest, DstURI, Parameters).
+send_proxy_request(Socket, Request, DstURI, Parameters) when record(Request, request) ->
+    CompatRequest = {Request#request.method, Request#request.uri, Request#request.header, Request#request.body},
+    send_proxy_request(Socket, CompatRequest, DstURI, Parameters);
+
+send_proxy_request(Socket, Request, DstURI, Parameters) ->
+    siprequest:send_proxy_request(Socket, Request, DstURI, Parameters).
 
 send_result(Header, Socket, Body, Code, Description) ->
     siprequest:send_result(Header, Socket, Body, Code, Description).
