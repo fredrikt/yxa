eb015a45698d787a82d0db489870fb39a87a988d
Fredrik Thulin <ft@it.su.se>
2004-04-05 08:26:20 +0000

    add IPv6 version of makeip/1, and add myip_list/0 that should return all our IPs
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@424 e454d3dc-12ff-0310-9b8b-94264392205f

--- siphost.erl
+++ siphost.erl
@@ -1,17 +1,35 @@
 -module(siphost).
--export([myip/0, makeip/1]).
+-export([myip/0, myip_list/0, makeip/1]).
 
-makeip(AddrIn) ->
-    {Addr1, Addr2, Addr3, Addr4} = AddrIn,
-    integer_to_list(Addr1) ++ "." ++
-	integer_to_list(Addr2) ++ "." ++
-	integer_to_list(Addr3) ++ "." ++
-	integer_to_list(Addr4).
+makeip({A1, A2, A3, A4}) ->
+    integer_to_list(A1) ++ "." ++
+	integer_to_list(A2) ++ "." ++
+	integer_to_list(A3) ++ "." ++
+	integer_to_list(A4);
+makeip({A1, A2, A3, A4, A5, A6, A7, A8}) ->
+    "[" ++
+	ipv6ify(A1) ++ ":" ++
+	ipv6ify(A2) ++ ":" ++
+	ipv6ify(A3) ++ ":" ++
+	ipv6ify(A4) ++ ":" ++
+	ipv6ify(A5) ++ ":" ++
+	ipv6ify(A6) ++ ":" ++
+	ipv6ify(A7) ++ ":" ++
+	ipv6ify(A8) ++ "]".
 
+ipv6ify(0) ->
+    "0";
+ipv6ify(A) ->
+    hex:to(A, 4).
 
 myip() ->
     [A | _] = get_iplist(),
     A.
+
+%% XXX make this return all addresses, currently IPv6 addresses
+%% are not returned!
+myip_list() ->
+    get_iplist().
 
 get_if([]) ->
     [];
