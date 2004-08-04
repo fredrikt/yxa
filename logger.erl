50bb206bf48ddf6aac27dbf78d13e0dddb1c8778
Fredrik Thulin <ft@it.su.se>
2004-08-04 22:51:44 +0000

    clean up the existing log rotation code and add rotating when log files grow too large (default is 250 MB) even though I think the long term solution is to check the need for rotation outside of Yxa and then send signals to the logger process.
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@493 e454d3dc-12ff-0310-9b8b-94264392205f

--- logger.erl
+++ logger.erl
@@ -8,6 +8,8 @@
 %%%-------------------------------------------------------------------
 -module(logger).
 
+%%-compile(export_all).
+
 -behaviour(gen_server).
 %%--------------------------------------------------------------------
 %% Include files
@@ -21,6 +23,9 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -record(state, {debug_iodev, debug_fn, normal_iodev, normal_fn, error_iodev, error_fn}).
+
+%% needed for file:read_file_info
+-include_lib("kernel/include/file.hrl").
 
 %%====================================================================
 %% External functions
@@ -54,6 +59,15 @@ start_link(AppName) ->
 %%          {stop, Reason}
 %%--------------------------------------------------------------------
 init([Basename]) ->
+    %% Check if we should rotate log files when they reach a upper
+    %% limit in size (bytes). Defaults to 250 MB.
+    case sipserver:get_env(max_log_filesize, 250 * 1024 * 1024) of
+	none -> ok;
+	Size ->
+	    %% Set up a timer to do the size checking every 60 seconds
+	    {ok, T} = timer:send_interval(60 * 1000, logger, {check_logfile_size, Size})
+    end,
+    %% Construct log filenames and open/create them
     DebugFn = lists:concat([Basename, ".debug"]),
     NormalFn = lists:concat([Basename, ".log"]),
     ErrorFn = lists:concat([Basename, ".error"]),
@@ -75,17 +89,13 @@ init([Basename]) ->
 %%          {stop, Reason, State}            (terminate/2 is called)
 %%--------------------------------------------------------------------
 handle_call({rotate_logs}, From, State) ->
-    %% Create rotation suffix from current time
-    {Megasec, Sec, USec} = now(),
-    USecStr = string:substr(integer_to_list(USec), 1, 3),
-    DateTime = util:sec_to_date(Megasec * 1000000 + Sec),
-    case string:tokens(DateTime, " ") of
-	[Date, Time] ->
-	    Suffix = "-" ++ Date ++ "_" ++ Time,
+    %% Create rotation filename suffix from current time
+    case create_filename_time_suffix() of
+	error ->
+	    {reply, {error, "Could not create suitable suffix"}, State};
+	Suffix when list(Suffix) ->
 	    {Res, NewState} = rotate([debug, normal, error], Suffix, State),
-	    {reply, Res, NewState};
-	_ ->
-	    {reply, {error, "Could not create suitable suffix"}, State}
+	    {reply, Res, NewState}
     end;
 
 handle_call({rotate_logs, Suffix}, From, State) ->
@@ -114,7 +124,7 @@ handle_cast({log, Level, TimeStamp, Format, Arguments, Pid}, State) ->
     NormalLevel = debug_level(normal),
     ErrorLevel = debug_level(error),
     NumLevel = debug_level(Level),
-    if 
+    if
 	NumLevel >= ErrorLevel ->
 	    log_to_stdout(Level, TimeStamp, Format, Arguments, Pid),
 	    log_to_device(State#state.error_iodev, Level, TimeStamp, Format, Arguments, Pid);
@@ -136,7 +146,12 @@ handle_cast({log, Level, TimeStamp, Format, Arguments, Pid}, State) ->
 	    log_to_device(State#state.debug_iodev, Level, TimeStamp, Format, Arguments, Pid);
 	true -> none
     end,
+    {noreply, State};
+
+handle_cast(Unknown, State) ->
+    logger:log(error, "Logger: Received unknown gen_server cast : ~p", [Unknown]),
     {noreply, State}.
+
 
 %%--------------------------------------------------------------------
 %% Function: handle_info/2
@@ -145,7 +160,26 @@ handle_cast({log, Level, TimeStamp, Format, Arguments, Pid}, State) ->
 %%          {noreply, State, Timeout} |
 %%          {stop, Reason, State}            (terminate/2 is called)
 %%--------------------------------------------------------------------
+handle_info({check_logfile_size, Size}, State) ->
+    %% If not configured not to, we get a check_logfile_size signal
+    %% every now and then. Check if any of our logfiles are over the
+    %% limit and if so, rotate it.
+    case needs_rotating([debug, normal, error], Size, State) of
+	[] ->
+	    {noreply, State};
+	L ->
+	    %% Create rotation filename suffix from current time
+	    case create_filename_time_suffix() of
+	        error ->
+		    {noreply, State};
+	        Suffix when list(Suffix) ->
+		    {_, NewState} = rotate(L, Suffix, State),
+		    {noreply, NewState}
+	    end
+    end;
+
 handle_info(Info, State) ->
+    logger:log(error, "Logger: Received unknown signal :~n~p", [Info]),
     {noreply, State}.
 
 %%--------------------------------------------------------------------
@@ -203,54 +237,81 @@ log_to_stdout(Level, TimeStamp, Format, Arguments, Pid) ->
     end,
     io:format("~n", []).
 
-%% Executed in caller pid, not in persistent logger process.
-do_log(Level, Format, Arguments) when atom(Level), list(Format), list(Arguments) ->
+%%--------------------------------------------------------------------
+%% Function: create_filename_time_suffix/0
+%% Description: Return a date string (european style) suitable for
+%%              appending to a filename when rotating it.
+%% Returns: String |
+%%          error
+%%--------------------------------------------------------------------
+create_filename_time_suffix() ->
     {Megasec, Sec, USec} = now(),
-    USecStr = string:substr(integer_to_list(USec), 1, 3),
     DateTime = util:sec_to_date(Megasec * 1000000 + Sec),
-    LogTS = lists:concat([DateTime, ".", USecStr]),
-    gen_server:cast(logger, {log, Level, LogTS, Format, Arguments, self()}),
-    ok.
+    case string:tokens(DateTime, " ") of
+	[Date, Time] ->
+	    Suffix = "-" ++ Date ++ "_" ++ Time,
+	    Suffix;
+	_ ->
+	    logger:log(error, "Logger: Failed creating a log file rotation suffix"),
+	    error
+    end.
 
+%%--------------------------------------------------------------------
+%% Function: needs_rotating/2
+%% Description: Check a list of {level, Fn} log files to see if any of
+%%              them are larger than Size bytes. Return a list of all
+%%              'level' atoms whose logfiles exceeds the limit.
+%% Returns: []          |
+%%          ListOfAtoms
+%%--------------------------------------------------------------------
+needs_rotating(In, Size, State) when record(State, state) ->
+    needs_rotating2(In, Size, State, []).
+
+needs_rotating2([], Size, State, Res) ->
+    lists:reverse(Res);
+needs_rotating2([H | T], Size, State, Res) when record(State, state) ->
+    Fn = level2filename(H, State),
+    case file:read_file_info(Fn) of
+	{ok, FileInfo} when record(FileInfo, file_info), FileInfo#file_info.size > Size ->
+	    %% file_info record returned, and size is larger than Size
+	    logger:log(debug, "Logger: Rotating '~p' logfile ~p since it is larger than ~p bytes (~p)",
+		       [H, Fn, Size, FileInfo#file_info.size]),
+	    needs_rotating2(T, Size, State, [H | Res]);
+	_ ->
+	    %% Either error returned from read_file_info, or file size is within limits.
+	    %% We don't check (care) which one...
+	    needs_rotating2(T, Size, State, Res)
+    end.
 
 rotate([], Suffix, State) when record(State, state) ->
     {ok, State};
-rotate([debug | T], Suffix, State) when record(State, state) ->
-    case rotate_file(State#state.debug_iodev, State#state.debug_fn, Suffix) of
+rotate([Level | T], Suffix, State) when record(State, state) ->
+    Fn = level2filename(Level, State),
+    case rotate_file(Fn, Suffix) of
 	{ok, NewIoDev} ->
-	    logger:log(debug, "Rotated debug logfile with suffix ~p", [Suffix]),
-	    rotate(T, Suffix, State#state{debug_iodev=NewIoDev});
+	    logger:log(debug, "Rotated '~p' logfile with suffix ~p", [Level, Suffix]),
+	    rotate(T, Suffix, update_iodev(Level, NewIoDev, State));
 	{error, E} ->
-	    logger:log(error, "Failed rotating debug logfile with suffix ~p : ~p", [Suffix, E]),
-	    {{error, "Failed rotating debug logfile"}, State}
-    end;
+	    logger:log(error, "Failed rotating '~p' logfile with suffix ~p : ~p", [Level, Suffix, E]),
+	    Str = lists:concat(["Failed rotating '", Level, "' logfile"]),
+	    {{error, Str}, State}
+    end.
 
-rotate([normal | T], Suffix, State) when record(State, state) ->
-    case rotate_file(State#state.normal_iodev, State#state.normal_fn, Suffix) of
-	{ok, NewIoDev} ->
-	    logger:log(debug, "Rotated normal logfile with suffix ~p", [Suffix]),
-	    rotate(T, Suffix, State#state{normal_iodev=NewIoDev});
-	{error, E} ->
-	    logger:log(error, "Failed rotating normal logfile with suffix ~p : ~p", [Suffix, E]),
-	    {{error, "Failed rotating normal logfile"}, State}
-    end;
+update_iodev(debug, N, State) when record(State, state) ->
+    State#state{debug_iodev=N};
+update_iodev(normal, N, State) when record(State, state) ->
+    State#state{normal_iodev=N};
+update_iodev(error, N, State) when record(State, state) ->
+    State#state{error_iodev=N}.
 
-rotate([error | T], Suffix, State) when record(State, state) ->
-    case rotate_file(State#state.error_iodev, State#state.error_fn, Suffix) of
-	{ok, NewIoDev} ->
-	    logger:log(debug, "Rotated error logfile with suffix ~p", [Suffix]),
-	    rotate(T, Suffix, State#state{error_iodev=NewIoDev});
-	{error, E} ->
-	    logger:log(error, "Failed rotating error logfile with suffix ~p : ~p", [Suffix, E]),
-	    {{error, "Failed rotating error logfile"}, State}
-    end;
+level2filename(debug, State) when record(State, state) ->
+    State#state.debug_fn;
+level2filename(normal, State) when record(State, state) ->
+    State#state.normal_fn;
+level2filename(error, State) when record(State, state) ->
+    State#state.error_fn.
 
-rotate([H | T], Suffix, State) when record(State, state) ->
-    logger:log(error, "Invalid log level ~p supplied to rotate", [H]),
-    {{error, "Invalid log level supplied to rotate"}, State}.
-
-
-rotate_file(IoDev, Filename, Suffix) ->
+rotate_file(Filename, Suffix) ->
     %% Open new file before we try to rename old one to make sure
     %% there isn't any permission problems etc. with us creating a new
     %% logfile
@@ -262,7 +323,7 @@ rotate_file(IoDev, Filename, Suffix) ->
 	    case file:rename(Filename, Filename ++ Suffix) of
 		ok ->
 		    %% Rename temporary file to the right filename
-		    %% XXX Can we trap errors here? 
+		    %% XXX Can we trap errors here?
 		    file:rename(TmpFile, Filename),
 		    {ok, NewIoDev};
 		_ ->
@@ -273,6 +334,17 @@ rotate_file(IoDev, Filename, Suffix) ->
 	_ ->
 	    error
     end.
+
+%% do_log is executed in caller pid (by log/2 or log/3), not in
+%% persistent logger process.
+do_log(Level, Format, Arguments) when atom(Level), list(Format), list(Arguments) ->
+    {Megasec, Sec, USec} = now(),
+    USecStr = string:substr(integer_to_list(USec), 1, 3),
+    DateTime = util:sec_to_date(Megasec * 1000000 + Sec),
+    LogTS = lists:concat([DateTime, ".", USecStr]),
+    gen_server:cast(logger, {log, Level, LogTS, Format, Arguments, self()}),
+    ok.
+
 
 %%--------------------------------------------------------------------
 %%% Interface functions
