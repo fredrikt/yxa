515eac74cd13687ffbae2c4c5950668120bfd482
Fredrik Thulin <ft@it.su.se>
2004-10-06 08:16:31 +0000

    documentation and indentation/formatting
    Committing for Hï¿½kan Stenholm <hsten@it.su.se>.
    
    
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@575 e454d3dc-12ff-0310-9b8b-94264392205f

--- util.erl
+++ util.erl
@@ -1,11 +1,62 @@
--module(util).
--export([timestamp/0, sec_to_date/1, isnumeric/1, regexp_rewrite/2, casecompare/2, casegrep/2, join/2, concat/2,
-	 safe_is_process_alive/1, safe_signal/3, remove_v6_brackets/1]).
+%%
+%%--------------------------------------------------------------------
 
+-module(util).
+
+%%--------------------------------------------------------------------
+%% External exports
+%%--------------------------------------------------------------------
+
+-export([
+	 timestamp/0,
+	 sec_to_date/1,
+	 isnumeric/1,
+	 regexp_rewrite/2,
+	 casecompare/2,
+	 casegrep/2,
+	 join/2,
+	 concat/2,
+	 safe_is_process_alive/1,
+	 safe_signal/3,
+	 remove_v6_brackets/1
+	]).
+
+%%--------------------------------------------------------------------
+%% Internal exports
+%%--------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% Include files
+%%--------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% Records
+%%--------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% Macros
+%%--------------------------------------------------------------------
+
+%%====================================================================
+%% External functions
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% Function: timestamp()
+%% Descrip.: number of seconds elapsed since base point of now()
+%% Returns : integer()
+%%--------------------------------------------------------------------
 timestamp() ->
     {Megasec, Sec, _} = now(),
     Megasec * 1000000 + Sec.
 
+%%--------------------------------------------------------------------
+%% Function: sec_to_date(Seconds)
+%%           Seconds = integer(), seconds since start point of now()
+%% Descrip.: takes a Seconds value (e.g. from timestamp) and returns a
+%%           nicely formatted string
+%% Returns : "yyyy-mm-dd hh:mm:ss" formated string
+%%--------------------------------------------------------------------
 sec_to_date(never) ->
     "never";
 sec_to_date(Seconds) when is_integer(Seconds) ->
@@ -16,8 +67,16 @@ localtime_to_string({{Year, Month, Day}, {Hour, Minute, Second}}) ->
     io_lib:format("~4..0w-~2..0w-~2..0w ~2..0w:~2..0w:~2..0w",
 		  [Year, Month, Day, Hour, Minute, Second]).
 
+%%--------------------------------------------------------------------
+%% Function: isnumeric(Number)
+%%           Number = string()
+%% Descrip.: determine if Number is a string containing only numbers,
+%%           length must be >= 1
+%% Returns : true | false
+%%--------------------------------------------------------------------
 isnumeric(Number) when is_list(Number) ->
-    case regexp:first_match(Number, "^[0-9]+$") of
+    Pattern = "^[0-9]+\$",
+    case regexp:first_match(Number, Pattern) of
 	{match, _, _} ->
 	    true;
 	_ ->
@@ -26,6 +85,11 @@ isnumeric(Number) when is_list(Number) ->
 isnumeric(_) ->
     false.
 
+%%--------------------------------------------------------------------
+%% Function:
+%% Descrip.:
+%% Returns :
+%%--------------------------------------------------------------------
 regexp_rewrite(Input, []) ->
     nomatch;
 
@@ -40,6 +104,9 @@ regexp_rewrite(Input, [{Regexp, Rewrite} | Rest]) ->
 	    []
     end.
 
+%% Function:
+%% Descrip.: ???
+%% Returns :
 apply_rewrite([], List) ->
     [];
 apply_rewrite([$\\, $\\ | Rest], List) ->
@@ -49,18 +116,28 @@ apply_rewrite([$\\, C | Rest], List) ->
 	error ->
 	    [C | apply_rewrite(Rest, List)];
 	0 ->
-	    [C | apply_rewrite(Rest, List)];	    
+	    [C | apply_rewrite(Rest, List)];
 	Number ->
 	    lists:nth(Number, List) ++ apply_rewrite(Rest, List)
     end;
 apply_rewrite([C | Rest], List) ->
     [C | apply_rewrite(Rest, List)].
 
+%% return integer value (0-9) of the chars $0-$9
+%% return error if Digit is a non-numerical char (or other type)
 digit(Digit) when is_integer(Digit), Digit >= $0, Digit =< $9 ->
     Digit - $0;
 digit(Digit) ->
     error.
 
+
+%%--------------------------------------------------------------------
+%% Function: casecompare(Str1, Str2)
+%%           Str1, Str2 = string() | none
+%% Descrip.: return true if Str1 and Str2 are either 'none' or the
+%%           same string (ignore case)
+%% Returns : true | false
+%%--------------------------------------------------------------------
 casecompare(none, none) ->
     true;
 casecompare(none, String) ->
@@ -76,17 +153,34 @@ casecompare(String1, String2) ->
 	    false
     end.
 
+%%--------------------------------------------------------------------
+%% Function: casegrep(Str, StrList)
+%%           Str = string() | none
+%%           StrList = list of string()
+%% Descrip.: determine if Str is a memeber of StrList
+%% Returns : true | false
+%%--------------------------------------------------------------------
 casegrep(String1,[]) ->
-	false;
-
+    false;
 casegrep(String1,[String2 | Rest]) ->
-	case casecompare(String1,String2) of
-		true -> 
-			true;
-		_ ->
-			casegrep(String1,Rest)
-	end.
-			 
+    case casecompare(String1,String2) of
+	true ->
+	    true;
+	_ ->
+	    casegrep(String1,Rest)
+    end.
+
+%%--------------------------------------------------------------------
+%% Function: join(Strings, Separator)
+%%           Strings = list of string()
+%%           Separator = string()
+%% Descrip.: create a single string from the Strings entries,
+%%           Separator is placed between all entries (but not after
+%%           the last one)
+%% Returns : string()
+%% XXX badly chosen function name - I mostly associate this name with
+%% functions like lists:append - hsten
+%%--------------------------------------------------------------------
 join([], Separator) ->
     [];
 join([A], Separator) ->
@@ -94,11 +188,29 @@ join([A], Separator) ->
 join([String | Rest], Separator) ->
     String ++ Separator ++ join(Rest, Separator).
 
+%%--------------------------------------------------------------------
+%% Function: concat(Strings, Separator)
+%%           Strings = list of string()
+%%           Separator = string()
+%% Descrip.: create a single string from the Strings entries, each
+%%           entry ends with the Separator.
+%% Returns : list()
+%% XXX badly chosen function name - I mostly associate this name with
+%% functions that add a element to a list - hsten
+%%--------------------------------------------------------------------
 concat([], Separator) ->
     [];
 concat([A | B], Separator) ->
     A ++ Separator ++ concat(B, Separator).
 
+%%--------------------------------------------------------------------
+%% Function: safe_is_process_alive(Process)
+%%           Process = pid() | atom()
+%% Descrip.: determine if the process Process is running
+%% Returns : {Alive, ProcessPid}
+%%           Alive = true | false
+%%           ProcessPid = pid()
+%%--------------------------------------------------------------------
 safe_is_process_alive(Pid) when is_pid(Pid) ->
     {is_process_alive(Pid), Pid};
 safe_is_process_alive(Name) when is_atom(Name) ->
@@ -114,6 +226,14 @@ safe_is_process_alive(Name) when is_atom(Name) ->
 	    {false, Pid}
     end.
 
+%%--------------------------------------------------------------------
+%% Function: safe_signal(LogTag, PidIn, Message)
+%%           LogTag = string() - application name (???)
+%%           PidIn = pid() | atom()
+%%           Message = string()
+%% Descrip.: ???
+%% Returns : ok | error
+%%--------------------------------------------------------------------
 safe_signal(LogTag, PidIn, Message) ->
     case util:safe_is_process_alive(PidIn) of
 	{true, Pid} ->
@@ -126,21 +246,41 @@ safe_signal(LogTag, PidIn, Message) ->
 	    error
     end.
 
-remove_v6_brackets([$[ | H]) ->
+%%--------------------------------------------------------------------
+%% Function: remove_v6_brackets(H)
+%%           H = string()
+%% Descrip.: ???
+%% Returns : ip6_address() | string()
+%%--------------------------------------------------------------------
+remove_v6_brackets([$[ | R] = H) ->
     %% Might be IPv6 formatted address, check if it ends with "]"
-    case string:substr(H, length(H), 1) of
+    case string:substr(R, length(R), 1) of
 	"]" ->
-	    A = string:substr(H, 1, length(H) - 1),
+	    A = string:substr(R, 1, length(R) - 1),
 	    case inet_parse:ipv6_address(A) of
 		{ok, _} ->
 		    A;
 		_ ->
 		    %% what was between [ and ] was not a valid v6 address
-		    "[" ++ A ++ "]"
+		    H
 	    end;
-	U ->
+	_ ->
 	    %% No ending "]"
-	    "[" ++ H
+	    H
     end;
 remove_v6_brackets(H) ->
     H.
+
+%%====================================================================
+%% Behaviour functions
+%%====================================================================
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% Function:
+%% Descrip.:
+%% Returns :
+%%--------------------------------------------------------------------
