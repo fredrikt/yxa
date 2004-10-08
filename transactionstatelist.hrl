67da2908804f2c9bcc7b327f21c62118991f762c
Fredrik Thulin <ft@it.su.se>
2004-10-08 06:15:07 +0000

    formatting
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@599 e454d3dc-12ff-0310-9b8b-94264392205f

--- transactionstatelist.hrl
+++ transactionstatelist.hrl
@@ -1,4 +1,22 @@
--record(transactionstatelist, {list}).
--record(transactionstate, {ref, type, id, ack_id, branch, pid, appdata,
-	request, response_to_tag, expire, stateless_response_branches}).
+%%
+%%
+%% -------------------------------------------------------------------
+
+-record(transactionstatelist, {
+	  list
+	 }).
+
+-record(transactionstate, {
+	  ref, 
+	  type, 
+	  id, 
+	  ack_id, 
+	  branch, 
+	  pid, 
+	  appdata,
+	  request, 
+	  response_to_tag, 
+	  expire, 
+	  stateless_response_branches
+	 }).
 
