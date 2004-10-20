8fbb32e4cd08793ff2744e0d75532dd610c003d5
Fredrik Thulin <ft@it.su.se>
2004-10-20 08:37:15 +0000

    add type checking of input variables. add some comments and code structure.
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@632 e454d3dc-12ff-0310-9b8b-94264392205f

--- transportlayer.erl
+++ transportlayer.erl
@@ -1,20 +1,53 @@
--module(transportlayer).
--export([start/0, send_proxy_request/4, send_proxy_response/2, send_result/5,
-	 send_result/6]).
+%%%-------------------------------------------------------------------
+%%% File    : transportlayer
+%%% Author  : Fredrik Thulin <ft@it.su.se>
+%%% Descrip.: Transport layer functions. Because of legacy reasons,
+%%%           the actual code is in siprequest.erl but the sending
+%%%           code will be moved here RSN.
+%%%
+%%% Created : 01 Mar 2004 by Fredrik Thulin <ft@it.su.se>
+%%%-------------------------------------------------------------------
 
+-module(transportlayer).
+
+%%--------------------------------------------------------------------
+%% External exports
+%%--------------------------------------------------------------------
+-export([
+	 start/0,
+	 send_proxy_request/4,
+	 send_proxy_response/2,
+	 send_result/5,
+	 send_result/6
+	]).
+
+%%--------------------------------------------------------------------
+%% Records
+%%--------------------------------------------------------------------
 -include("siprecords.hrl").
+-include("sipsocket.hrl").
+
+%%====================================================================
+%% External functions
+%%====================================================================
 
 start() ->
     sipsocket:start_link().
 
-send_proxy_response(Socket, Response) when record(Response, response) ->
+send_proxy_response(Socket, Response)
+  when is_record(Socket, sipsocket); Socket == none, is_record(Response, response) ->
     siprequest:send_proxy_response(Socket, Response).
 
-send_proxy_request(Socket, Request, DstURI, Parameters) when record(Request, request) ->
-    siprequest:send_proxy_request(Socket, Request, DstURI, Parameters).
+send_proxy_request(Socket, Request, DstList, Parameters)
+  when is_record(Socket, sipsocket); Socket == none, is_record(Request, request) ->
+    siprequest:send_proxy_request(Socket, Request, DstList, Parameters).
 
-send_result(RequestHeader, Socket, Body, Code, Description) ->
-    siprequest:send_result(RequestHeader, Socket, Body, Code, Description).
+send_result(RequestHeader, Socket, Body, Status, Reason)
+  when is_record(RequestHeader, keylist), is_record(Socket, sipsocket); Socket == none,
+       is_list(Body), is_integer(Status), is_list(Reason) ->
+    siprequest:send_result(RequestHeader, Socket, Body, Status, Reason).
 
-send_result(RequestHeader, Socket, Body, Code, Description, ExtraHeaders) ->
-    siprequest:send_result(RequestHeader, Socket, Body, Code, Description, ExtraHeaders).
+send_result(RequestHeader, Socket, Body, Status, Reason, ExtraHeaders)
+  when is_record(RequestHeader, keylist), is_record(Socket, sipsocket); Socket == none,
+       is_list(Body), is_integer(Status), is_list(Reason), is_record(ExtraHeaders, keylist) ->
+    siprequest:send_result(RequestHeader, Socket, Body, Status, Reason, ExtraHeaders).
