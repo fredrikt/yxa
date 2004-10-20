ee2fef4c57b13788c3c406d6fe4c682f61a62ba3
Fredrik Thulin <ft@it.su.se>
2004-10-20 08:46:36 +0000

    move keylist record here from keylist.erl to facilitate type checking outside of keylist.erl
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@634 e454d3dc-12ff-0310-9b8b-94264392205f

--- siprecords.hrl
+++ siprecords.hrl
@@ -5,3 +5,14 @@
 -record(via, {proto, host, port, param}).
 
 -record(sipurl, {proto, user, pass, host, port, param}).
+
+%%--------------------------------------------------------------------
+%% keylist.erl records 
+%%--------------------------------------------------------------------
+
+%% a list wrapped in a record to allow better type checking and 
+%% limiting direct access to it (use access functions supplied by
+%% module keylist)
+-record(keylist, {
+	  list     % list() of keyelem record() with unique keys
+	 }).
