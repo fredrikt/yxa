a28174bcb143a6047d9593fdf956c0730396c275
Fredrik Thulin <ft@it.su.se>
2004-11-18 07:47:53 +0000

    add comments
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@647 e454d3dc-12ff-0310-9b8b-94264392205f

--- sipsocket.hrl
+++ sipsocket.hrl
@@ -2,25 +2,26 @@
 %%--------------------------------------------------------------------
 
 -record(sipsocket, {
-	  module, 
-	  proto, 
-	  pid, 
-	  data
+	  module, % atom(), sipsocket module name to use for this socket
+	  proto,  % atom(), tcp|tcp6|udp|udp6|tls|tls6 ...
+	  pid,    % pid(), socket connection handler
+	  data    % term()
 	 }).
 
 %% sipdst is the Transport Layer destination record.
 -record(sipdst, {
-	  proto, 
-	  addr, 
-	  port, % integer() (siprequest.erl - are other formats ok ? 
-		%            they won't work in get_response_socket/4)
-	  uri
+	  proto, % string() lower case, e.g. "sip" | ... , protocol
+	  addr,  % string() | sipurl record() ?
+	  port,  % integer() (siprequest.erl - are other formats ok ? 
+		 %            they won't work in get_response_socket/4)
+	  uri    % sipurl record() ?
 	 }).
 
 -record(siporigin, {
-	  proto, 
-	  addr, 
-	  port, 
-	  receiver, 
-	  sipsocket
+	  proto,    % string() lower case, e.g. "sip" | ... , protocol
+	  addr,     % string() | sipurl record() ?
+	  port,     % string() | integer()  ?
+	  receiver, % string() | sipurl record() ?
+	  sipsocket % sipsocket record() ?
 	 }).
+
