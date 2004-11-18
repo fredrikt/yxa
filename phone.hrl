1b6964bd152bfad0bf775aab70e18385886421f1
Fredrik Thulin <ft@it.su.se>
2004-11-18 09:22:15 +0000

    update phone record to include call-id and cseq (needed for RFC3261 compliance)
    committing for Hï¿½kan Stenholm <hsten@it.su.se>
    
    
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@673 e454d3dc-12ff-0310-9b8b-94264392205f

--- phone.hrl
+++ phone.hrl
@@ -1,19 +1,38 @@
 %%
 %% -------------------------------------------------------------------
 
+%% -------------------------------------------------------------------
 %% Table 'phone' is the location database. One entry for every phone
 %% (or, more correctly, User Agent) that REGISTERs for one of our
 %% users.
--record(phone, {
-	  number,
-	  flags, 
-	  class, 
-	  expire, 
-	  address, 
-	  requristr
+%% phone record is used to map a user (number) to one UAC (address and
+%% requristr). Several UACs can be configured to produce the same 
+%% user (number) by sending the same "To" field in a REGISTER request,
+%% thereby mapping several UACs to the same user. 
+%% -------------------------------------------------------------------
+-record(phone, {     % location - should be a better record name
+	  number,    % string(), sipuser authenitcation name (from sipuserdb:get_user_with_address(...))
+	             % user - would be a better name
+	             % acts as "adress of record" (AOR) (see RFC 3261 chapter 10 p65)
+	  flags,     % list of {Name,Value}
+                     % Name  = atom()
+                     % Value = term()
+	  class,     % dynamic | static | ... 
+	  expire,    % integer(), util:timestap/0 value, stores the time at which the phone database entry
+	             % is no longer to be used and should be removed
+	             % Note: this differs from the expire value used in the REGISTER request (see RFC 3261)
+	             % which is used to indicate how long a registratio is to last i.e. 
+	             % "phone.expire = time of registration + request expire"
+	  address,   % string(), sip url with parameters
+	  requristr, % string(), sip url
+	  callid,    % string(), a sequence of bytes
+	  cseq       % integer()
 	 }).
 
-%% Table 'user' is the user database if you are using sipuserdb_mnesia.
+%% -------------------------------------------------------------------
+%% Table 'user' is the user database if you are using 
+%% sipuserdb_mnesia.
+%% -------------------------------------------------------------------
 -record(user, {
 	  user, 
 	  password, 
@@ -22,10 +41,12 @@
 	  classes
 	 }).
 
+%% -------------------------------------------------------------------
 %% Table 'numbers' maps extra addresses (besides the first one -
-%% ('number' in table 'user') to users. Note well that sipuserdb_mnesia
-%% also has implicit mappings of addresses to users through
-%% lookup:lookup_url_to_addresses.
+%% ('number' in table 'user') to users. Note well that 
+%% sipuserdb_mnesia also has implicit mappings of addresses to users 
+%% through lookup:lookup_url_to_addresses.
+%% -------------------------------------------------------------------
 -record(numbers, {
 	  user, 
 	  number
