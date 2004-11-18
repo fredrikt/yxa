400294aedd5d9a0c2235c74bef98a4207b576c6d
Fredrik Thulin <ft@it.su.se>
2004-11-18 07:51:35 +0000

    add element 'proto' to socketlistelem record, and add some comments
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@648 e454d3dc-12ff-0310-9b8b-94264392205f

--- socketlist.hrl
+++ socketlist.hrl
@@ -6,12 +6,12 @@
 	 }).
 
 -record(socketlistelem, {
-	  ref, 
-	  id, 
-	  pid, 
-	  local, 
-	  remote, 
-	  sipsocket, 
-	  expire
+	  ref,		%% unique reference
+	  id,		%% {listener, Proto, Port} | {in, Proto, Remote} | {out, Proto, Remote}
+	  pid,		%% pid() of connection handler process
+	  proto,	%% atom()
+	  local,	%% {IP, Port} tuple() 
+	  remote,	%% {IP, Port} tuple()
+	  sipsocket,	%% term(), socket
+	  expire	%% integer(), when this socket expires or 0 for never expire
 	 }).
-
