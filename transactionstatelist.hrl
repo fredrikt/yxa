19b67dbb3e4aa70b2fdc0bb9ba2eb8c0ad05b125
Fredrik Thulin <ft@it.su.se>
2004-11-18 08:13:22 +0000

    add comments, and add fields 'description' and 'result' to transactionstate record
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@652 e454d3dc-12ff-0310-9b8b-94264392205f

--- transactionstatelist.hrl
+++ transactionstatelist.hrl
@@ -5,21 +5,21 @@
 %% Container record to make sure noone modifies a transaction list
 %% without using functions exported by transactionstatelist module
 -record(transactionstatelist, {
-	  list
+	  list,
+	  tables
 	 }).
 
 %% Transaction layer data about ongoing transactions
 -record(transactionstate, {
-	  ref, 
-	  type, 
-	  id, 
-	  ack_id, 
-	  branch, 
-	  pid, 
-	  appdata,
-	  request, 
-	  response_to_tag, 
-	  expire, 
-	  stateless_response_branches
+	  ref,		%% unique reference 
+	  type,		%% server | client 
+	  id,		%% identification data for matching requests/responses to this transaction 
+	  ack_id,	%% special id for matching ACK to INVITE 
+	  pid,		%% pid() of transaction handler process
+	  appdata,	%% term(), data about this transaction stored by an application
+	  response_to_tag, %% the To-tag used in responses to this transaction that we have generated ourselves
+	  expire,	%% integer(), when in time this transaction expires. XXX UNUSED!
+	  stateless_response_branches=[], %% list() of string(), branches we have used when proxying this request on statelessly - DONT USE
+	  description,	%% string(), just used for displaying (in stack_monitor and debug logs etc.)
+	  result=none	%% string(), just used for displaying (in stack_monitor and debug logs etc.)
 	 }).
-
