7c2ed387b95d57243430d57faec32eee884cf3e7
Fredrik Thulin <ft@it.su.se>
2004-11-18 07:43:32 +0000

    update documentation and avoid compiler warnings about unused variables
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@643 e454d3dc-12ff-0310-9b8b-94264392205f

--- sipuserdb_mnesia.erl
+++ sipuserdb_mnesia.erl
@@ -1,4 +1,16 @@
+%%%-------------------------------------------------------------------
+%%% File    : sipuserdb_ldap.erl
+%%% Author  : Fredrik Thulin
+%%% Descrip.: A sipuserdb module with a Mnesia backend. Uses the ol'
+%%%           functions in phone.erl of course.
+%%%
+%%% Created : 30 Sep 2003 by Fredrik Thulin <ft@it.su.se>
+%%%-------------------------------------------------------------------
 -module(sipuserdb_mnesia).
+
+%%--------------------------------------------------------------------
+%% External exports
+%%--------------------------------------------------------------------
 -export([yxa_init/0,
 	 get_user_with_address/1,
 	 get_users_for_address_of_record/1,
@@ -13,26 +25,38 @@
 	 get_forward_for_user/1
 	]).
 
+%%--------------------------------------------------------------------
+%% Include files
+%%--------------------------------------------------------------------
 -include("siprecords.hrl").
 
-%% Mnesia userdb module
-
-%% Function: yxa_init/0
-%% Description: Perform any necessary startup initialization and
-%%              return an OTP supervisor child spec if we want to add
-%%              to sipserver_sup's list. If this sipuserdb_module
-%%              needs to be persistent, it should be a gen_server and
-%%              init should just return a spec so that the gen_server
-%%              is started by the supervisor.
-%% Returns: Spec |
-%%          []
+%%--------------------------------------------------------------------
+%% Function: yxa_init()
+%% Descrip.: Perform any necessary startup initialization and
+%%           return an OTP supervisor child spec if we want to add
+%%           to sipserver_sup's list. If this sipuserdb_module
+%%           needs to be persistent, it should be a gen_server and
+%%           init should just return a spec so that the gen_server
+%%           is started by the supervisor.
+%% Returns : Spec |
+%%           []
+%%           Spec = OTP supervisor child specification
 %%--------------------------------------------------------------------
 yxa_init() ->
     [].
 
-%% Looks up exactly one user with an Address. Used
-%% for example in REGISTER. If there are multiple
-%% users with an address, this function returns 'error'.
+%%--------------------------------------------------------------------
+%% Function: get_user_with_address(Address)
+%%           Address = string(), an address in string format.
+%% Descrip.: Looks up exactly one user with an Address. Used for
+%%           example in REGISTER. If there are multiple users with
+%%           this address in our database, this function returns
+%%           'error'.
+%% Returns:  Username |
+%%           nomatch  |
+%%           error
+%%           Username = string()
+%%--------------------------------------------------------------------
 get_user_with_address(Address) ->
     case phone:get_user(Address) of
 	{atomic, []} ->
@@ -50,7 +74,7 @@ get_user_with_address(Address) ->
 			       [Address, Unknown]),
 		    error
 	    end;
-	{atomic, [User]} ->
+	{atomic, [_User]} ->
 	    %% There exists exactly one user with this name. Return the name of the user,
 	    %% not the user record returned from phone:get_user().
 	    Address;
@@ -63,14 +87,21 @@ get_user_with_address(Address) ->
 	    error
     end.
 
-%% Looks up all users with a given address. Used
-%% to find out to which users we should send a request.
-%% Other userdb types than mnesia might perform multiple lookups.
+%%--------------------------------------------------------------------
+%% Function: get_users_for_address_of_record(Address)
+%%           Address = string(), an address in string format.
+%% Descrip.: Get all usernames of users matching an address. Used to
+%%           find out to which users we should send a request.
+%% Returns : Users   |
+%%           nomatch |
+%%           error
+%%           Users = list() of string() 
+%%--------------------------------------------------------------------
 get_users_for_address_of_record(Address) ->
     UserList = case phone:get_user(Address) of
 		   {atomic, []} ->
 		       [];
-		   {atomic, User} ->
+		   {atomic, _User} ->
 		       %% There exists a user with this name
 		       [Address];
 		   Unknown1 ->
@@ -94,6 +125,18 @@ get_users_for_address_of_record(Address) ->
 	    end
     end.
 
+%%--------------------------------------------------------------------
+%% Function: get_users_for_addresses_of_record(Addresses)
+%%           Addresses = list() of string(), addresses in string
+%%                       format.
+%% Descrip.: Iterate over a list of addresses of record, return
+%%           all users matching one or more of the addresses,
+%%           without duplicates.
+%% Returns : Users   |
+%%           nomatch |
+%%           error
+%%           Users = list() of string() 
+%%--------------------------------------------------------------------
 get_users_for_addresses_of_record(In) ->
     get_users_for_addresses_of_record2(In, []).
 
@@ -108,11 +151,16 @@ get_users_for_addresses_of_record2([H | T], Res) ->
     end.
 
 
-%% Function: get_addresses_for_users/1
-%% Description: Iterate over a list of users, return all their
-%%              addresses without duplicates. Uses the next function,
-%%              get_addresses_for_user/1.
-%% Returns: ListOfAddresses
+%%--------------------------------------------------------------------
+%% Function: get_addresses_for_users(UserList)
+%%           UserList = list() of string(), usernames
+%% Descrip.: Iterate over a list of users, return all their
+%%           addresses without duplicates.
+%%           Uses the next function, get_addresses_for_user/1.
+%% Returns : Addresses |
+%%           nomatch   |
+%%           error
+%%           Addresses = list() of string()
 %%--------------------------------------------------------------------
 get_addresses_for_users([]) ->
     [];
@@ -120,15 +168,23 @@ get_addresses_for_users([User | Rest]) ->
     lists:append(get_addresses_for_user(User), get_addresses_for_users(Rest)).
 
 
-%% Gets all addresses for a user. Used for example
-%% to check if a request from a user has an acceptable
-%% From: header.
+%%--------------------------------------------------------------------
+%% Function: get_addresses_for_user(User)
+%%           User = string()
+%% Descrip.: Get all possible addresses of a user. Both configured
+%%           ones, and implicit ones. Used for example to check if a
+%%           request from a user has an acceptable From: header.
+%% Returns : Addresses |
+%%           error
+%%           Addresses = list() of string()
+%%--------------------------------------------------------------------
 get_addresses_for_user(User) ->
     case phone:get_user(User) of
 	{atomic, []} ->
 	    logger:log(debug, "userdb-mnesia: No such user ~p", [User]),
 	    nomatch;
-	{atomic, [UserEntry]} ->
+	{atomic, [_UserEntry]} ->
+	    %% Ok, user exists
 	    case phone:get_numbers_for_user(User) of
 		{atomic, []} ->
 		    logger:log(debug, "userdb-mnesia: No numbers for user ~p", [User]),
@@ -152,12 +208,16 @@ get_addresses_for_user(User) ->
     end.
 
 
-%% Function: get_users_for_url/1
-%% Description: Given an URL that is typically the Request-URI of an
-%%              incoming request, make a list of implicit user
-%%              addresses and return a list of all users matching any
-%%              of these addresses.
-%% Returns: ListOfUsernames
+%%--------------------------------------------------------------------
+%% Function: get_users_for_url(URL)
+%%           URL = sipurl record()
+%% Descrip.: Given an URL that is typically the Request-URI of an
+%%           incoming request, make a list of implicit user
+%%           addresses and return a list of all users matching any
+%%           of these addresses. This is located in here since
+%%           user database backends can have their own way of
+%%           deriving addresses from a Request-URI.
+%% Returns : Usernames, list() of string()
 %%--------------------------------------------------------------------
 get_users_for_url(URL) when record(URL, sipurl) ->
     Addresses = local:lookup_url_to_addresses(sipuserdb_mnesia, URL),
@@ -169,13 +229,22 @@ get_users_for_url(URL) when record(URL, sipurl) ->
 %% Attribute fetching functions :
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%%--------------------------------------------------------------------
+%% Function: get_password_for_user(Username)
+%%           Username = string()
+%% Descrip.: Returns the password for a user.
+%% Returns : Password |
+%%           nomatch  |
+%%           error
+%%           Password = string()
+%%--------------------------------------------------------------------
 get_password_for_user(User) ->
     case phone:get_user(User) of
 	{atomic, []} ->
 	    logger:log(debug, "userdb-mnesia: No such user ~p when fetching password", [User]),
 	    nomatch;
 	{atomic, [A]} ->
-	    {Password, Flags, Classes} = A,
+	    {Password, _Flags, _Classes} = A,
 	    Password;
 	Unknown ->
 	    logger:log(error, "userdb-mnesia: Unexpected result from phone:get_user(), user ~p result : ~p",
@@ -183,13 +252,23 @@ get_password_for_user(User) ->
 	    error
     end.
 
+%%--------------------------------------------------------------------
+%% Function: get_classes_for_user(Username)
+%%           Username = string()
+%% Descrip.: Return a list of classes for this Username. Classes are
+%%           'free-form' atoms of types of PSTN destinations this user
+%%           is allowed to call to, through a pstnproxy. What class a
+%%           PSTN number is in is determined through pstnproxy
+%%           configuration.
+%% Returns : Classes, list() of atom()
+%%--------------------------------------------------------------------
 get_classes_for_user(User) ->
     case phone:get_user(User) of
 	{atomic, []} ->
 	    logger:log(debug, "userdb-mnesia: No such user ~p when fetching password", [User]),
 	    nomatch;
 	{atomic, [A]} ->
-	    {Password, Flags, Classes} = A,
+	    {_Password, _Flags, Classes} = A,
 	    Classes;
 	Unknown ->
 	    logger:log(error, "userdb-mnesia: Unexpected result from phone:get_user(), user ~p result : ~p",
@@ -197,6 +276,18 @@ get_classes_for_user(User) ->
 	    error
     end.
 
+%%--------------------------------------------------------------------
+%% Function: get_telephonenumber_for_user(User)
+%%           User = string()
+%% Descrip.: Return the telephone number for a user. We do this by
+%%           fetching all addresses for the user and then examining
+%%           them to see if any of them is a tel: URL, or has a
+%%           user part which is all numeric or is an E.164 number.
+%%           The numbering plan in the number return is not specified.
+%% Returns : Number  |
+%%           nomatch
+%%           Number = string()
+%%--------------------------------------------------------------------
 get_telephonenumber_for_user(User) ->
     case phone:get_numbers_for_user(User) of
 	{atomic, [FirstNumber | _]} ->
@@ -205,6 +296,15 @@ get_telephonenumber_for_user(User) ->
 	    nomatch
     end.
 
+%%--------------------------------------------------------------------
+%% Function: get_forwards_for_users(Usernames)
+%%           Usernames = list() of string()
+%% Descrip.: Return a list of forwards for this user. Forwards are
+%%           currently not in the main Yxa CVS repository - Magnus has
+%%           some CVS merging to do.
+%% Returns : list() of Unknown |
+%%           nomatch
+%%--------------------------------------------------------------------
 get_forwards_for_users(In) ->
     get_forwards_for_users2(In, []).
 
@@ -220,6 +320,15 @@ get_forwards_for_users2([H | T], Res) ->
 	    get_forwards_for_users2(T, lists:append(Res, [Fwd]))
     end.
 
+%%--------------------------------------------------------------------
+%% Function: get_forward_for_user(Username)
+%%           Username = string()
+%% Descrip.: Return the forward entry for a user. Forwards are
+%%           currently not in the main Yxa CVS repository - Magnus has
+%%           some CVS merging to do.
+%% Returns : list() of Unknown |
+%%           nomatch
+%%--------------------------------------------------------------------
 get_forward_for_user(User) ->
     case database_forward:fetch(User) of
 	{atomic, []} ->
