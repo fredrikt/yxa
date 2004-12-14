f4df05804f1be4f68b6a4da63794173e58bb40b2
Fredrik Thulin <ft@it.su.se>
2004-12-14 10:29:01 +0000

    implement start_transportlayer (for readability) and my_start_children (for robustness).
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@754 e454d3dc-12ff-0310-9b8b-94264392205f

--- sipserver_sup.erl
+++ sipserver_sup.erl
@@ -15,7 +15,11 @@
 %%--------------------------------------------------------------------
 %% External exports
 %%--------------------------------------------------------------------
--export([start_link/2]).
+-export([
+	 start_link/1,
+	 start_extras/2,
+	 start_transportlayer/1
+	]).
 
 %%--------------------------------------------------------------------
 %% Internal exports
@@ -38,21 +42,20 @@
 %% Function: start_link/3
 %% Description: Starts the supervisor
 %%--------------------------------------------------------------------
-start_link(AppCallbacks, AppSupdata) ->
-    supervisor:start_link({local, ?SERVER}, ?MODULE, [AppCallbacks, AppSupdata]).
+start_link(AppCallbacks) ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, [AppCallbacks]).
 
 %%====================================================================
 %% Server functions
 %%====================================================================
 %%--------------------------------------------------------------------
-%% Function: init([AppModule, AppSupdata])
+%% Function: init([AppModule])
 %%           AppModule = atom(), name of Yxa application module
-%%           AppSupdata = xxx
 %% Returns : {ok,  {SupFlags,  [ChildSpec]}} |
 %%           ignore                          |
 %%           {error, Reason}   
 %%--------------------------------------------------------------------
-init([AppModule, AppSupdata]) ->
+init([AppModule]) ->
     Logger = {logger, {logger, start_link, []},
                  permanent, 2000, worker, [logger]},
     Directory = {directory, {directory, start_link, []},
@@ -60,16 +63,50 @@ init([AppModule, AppSupdata]) ->
     TransactionLayer = {transactionlayer,
 			{transactionlayer, start_link, [AppModule]},
 			permanent, 2000, worker, [transactionlayer]},
+    MyList = [Logger, Directory, TransactionLayer],
+    {ok, {{one_for_one, 20, 60}, MyList}}.
+
+start_extras(Supervisor, AppSupdata) ->
     UserDb = sipuserdb:yxa_init(),
-    MyList = lists:append([Logger, Directory, TransactionLayer], UserDb),
+    MyList = UserDb,
     SupList = case AppSupdata of
-		  {append, AppList} when list(AppList) ->
+		  {append, AppList} when is_list(AppList) ->
 		      lists:append(MyList, AppList);
 		  none ->
 		      MyList
 	      end,
-    {ok,{{one_for_one,20,60}, SupList}}.
+    my_start_children(Supervisor, SupList).
+
+start_transportlayer(Supervisor) ->
+    %% Start the transport layer now that we have initialized everything
+    TransportLayer = {transportlayer,
+		      {transportlayer, start_link, []},
+		      permanent, 2000, supervisor, [transportlayer]},
+    my_start_children(Supervisor, [TransportLayer]).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+my_start_children(Supervisor, []) ->
+    {ok, Supervisor};
+my_start_children(Supervisor, [H|T]) ->
+    case supervisor:start_child(Supervisor, H) of
+	{error, E} ->
+	    Msg = lists:flatten(
+		    io_lib:format("Sipserver supervisor: Failed starting child '~p': ~p",
+				  [element(1, H), E])
+		    ),
+	    logger:log(error, Msg),
+	    %% We must sleep a few seconds here, so that the supervisor does not shut down
+	    %% the logger process while it is still logging (yes, that was what happened
+	    %% with the result being that the logger process crashed and produced a mis-
+	    %% guiding error 'ebadf' from io:format()).
+	    timer:sleep(3 * 1000),
+	    {error, Msg};
+	{ok, _} ->
+	    my_start_children(Supervisor, T);
+	{ok, _, _} ->
+	    my_start_children(Supervisor, T)
+    end.
+
+
