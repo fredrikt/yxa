51253d16c1109e83e6462532ff8b5a06416da0f8
Fredrik Thulin <ft@it.su.se>
2004-12-14 10:33:00 +0000

    parse_hostport: fix parsing of IPv6 address without enclosing brackets make ports be integers
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@757 e454d3dc-12ff-0310-9b8b-94264392205f

--- sipparse_util.erl
+++ sipparse_util.erl
@@ -1,4 +1,4 @@
-%% This module implements some of the parsing rules needed for
+% This module implements some of the parsing rules needed for
 %% the BNF defined in RFC 3261 (chapter 25 page 218)
 %%
 %% Note: The functions in this module are assumed to conform STRICTLY
@@ -6,6 +6,7 @@
 %%--------------------------------------------------------------------
 
 -module(sipparse_util).
+%%-compile(export_all).
 
 %%--------------------------------------------------------------------
 %% External exports
@@ -117,15 +118,15 @@ parse_host(Host) ->
 %% hostname
 %% ipv4address:port
 %% ipv4address
-%% ipv6reference:port
+%% [ipv6reference]:port
 %% ipv6reference
 %%
 %% Descrip.: splits the string into it's two parts, a exception is
 %%           thrown if the Host or Port is malformed
 %% Returns : {Host, Port} | throw() if parse fails
 %%           Host = string()
-%%           Port = string() | none
-%%           throw() = {error, Reason} | {'EXIT',Reason}
+%%           Port = integer() | none
+%%           throw() = {error, Reason} | {'EXIT', Reason}
 %%--------------------------------------------------------------------
 parse_hostport(HostPort) ->
     H = hd(HostPort),
@@ -141,7 +142,7 @@ parse_hostport(HostPort) ->
 		true ->
 		    %% probably a ipv4 address
 		    case catch parse_ipv4hostport(HostPort) of
-			{ok, Host, Port} when is_list(Host), is_list(Port); Port == none ->
+			{ok, Host, Port} when is_list(Host), is_integer(Port); Port == none ->
 			    {Host, Port};
 			_ ->
 			    %% was not IPv4 address, try to parse as IPv6 address without
@@ -166,14 +167,9 @@ parse_hostport_v6_or_hostname(In) ->
 
 parse_ipv6hostport([91 | IPv6Hostport]) ->	%% 91 is $[
     {IPv6Ref, Port} = get_ipv6ref_and_port(IPv6Hostport),
-    case Port of
-	none -> ok;
-	_ -> 
-	    %% throw if not a numeric string
-	    list_to_integer(Port)
-    end,
     %% throw if not ip6 ref
     is_IPv6reference(IPv6Ref),
+    %% XXX return without brackets?
     {ok, "[" ++ IPv6Ref ++ "]", Port};
 parse_ipv6hostport(In) ->
     %% throw if not ip6 ref
@@ -197,17 +193,15 @@ get_ipv6ref_and_port(IPv6Hostport) ->
 	{Host, PortRest} ->
 	    %% a style "[...]:Port"
 	    [$: | Port] = PortRest,
-	    {Host, Port}
+	    {Host, list_to_integer(Port)}
     end.
 
 
 parse_ipv4hostport(HostPort) ->
     case sipparse_util:split_fields(HostPort, $:) of
 	{Host, Port} ->
-	    %% throw if not a numeric string
-	    list_to_integer(Port),
-	    %% throw if not a IPv4 address string
-	    {ok, is_IPv4address(Host), Port};
+	    %% throw if not a IPv4 address string, or not a numeric port
+	    {ok, is_IPv4address(Host), list_to_integer(Port)};
 	{Host} ->
 	    %% throw if not a IPv4 address string
 	    {ok, is_IPv4address(Host), none}
@@ -217,11 +211,9 @@ parse_ipv4hostport(HostPort) ->
 parse_domain_hostport(HostPort) ->
     case sipparse_util:split_fields(HostPort, $:) of
 	{Host, Port} ->
-	    %% throw if not a numeric string
-	    list_to_integer(Port),
 	    %% throw if not a hostname
 	    is_hostname(Host),
-	    {ok, rm_trailing_dot_from_hostname(Host), Port};
+	    {ok, rm_trailing_dot_from_hostname(Host), list_to_integer(Port)};
 	{Host} ->
 	    %% throw if not a hostname
 	    is_hostname(Host),
@@ -333,7 +325,7 @@ is_IPv4address(Host) ->
 is_IPv6reference(IPv6Str) ->
     case inet_parse:ipv6_address(IPv6Str) of
 	{ok, _IPv6AsTuple} -> IPv6Str;
-	{error, _Reason} -> throw({error, not_a_IPv6_reference})
+	{error, _Reason} -> throw({error, not_an_IPv6_reference})
     end.
 
 
@@ -405,22 +397,22 @@ test() ->
     %%--------------------------------------------------------------------
     %% regular test case
     io:format("test: parse_hostport/1 - 1~n"),
-    {"foo.bar", "42"} = sipparse_util:parse_hostport("foo.bar:42"),
+    {"foo.bar", 42} = sipparse_util:parse_hostport("foo.bar:42"),
     %% test trailing "." in host name
     io:format("test: parse_hostport/1 - 2~n"),
-    {"foo", "42"} = sipparse_util:parse_hostport("foo.:42"),
+    {"foo", 42} = sipparse_util:parse_hostport("foo.:42"),
     %% trailing "." and no port
     io:format("test: parse_hostport/1 - 3~n"),
     {"foo", none} = sipparse_util:parse_hostport("foo."),
     %% IPv4 host and port
     io:format("test: parse_hostport/1 - 4~n"),
-    {"1.1.1.1", "42"} = sipparse_util:parse_hostport("1.1.1.1:42"),
+    {"1.1.1.1", 42} = sipparse_util:parse_hostport("1.1.1.1:42"),
     %% IPv4 host and no port
     io:format("test: parse_hostport/1 - 5~n"),
     {"1.1.1.1", none} = sipparse_util:parse_hostport("1.1.1.1"),
     %% IPv6 host and port
     io:format("test: parse_hostport/1 - 6~n"),
-    {"[1:2:3:4:5:6:7:8]", "42"} = sipparse_util:parse_hostport("[1:2:3:4:5:6:7:8]:42"),
+    {"[1:2:3:4:5:6:7:8]", 42} = sipparse_util:parse_hostport("[1:2:3:4:5:6:7:8]:42"),
     %% IPv6 host and no port
     io:format("test: parse_hostport/1 - 7~n"),
     {"[1:2:3:4:5:6:7:8]", none} = sipparse_util:parse_hostport("[1:2:3:4:5:6:7:8]"),
@@ -443,7 +435,7 @@ test() ->
     io:format("test: parse_hostport/1 - 13~n"),
     {"[abcd:2::6:BCDE:8]", none} = sipparse_util:parse_hostport("abcd:2::6:BCDE:8"),
     %% test bad port value
-    io:format("test: parse_hostport/1 - 14~n"),
+    io:format("test: parse_hostport/1 - 8~n"),
     case catch sipparse_util:parse_hostport("1.1.1.1:1A1") of
 	{'EXIT', _} -> ok;
 	{error, _} -> ok;
