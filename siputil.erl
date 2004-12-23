e2f2ee9742e02b494fb0ac50eb5113b202c31e79
Fredrik Thulin <ft@it.su.se>
2004-12-23 15:01:00 +0000

    linefix: make tail recursive. leads to 10% faster parsing of SIP messages.
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@767 e454d3dc-12ff-0310-9b8b-94264392205f

--- siputil.erl
+++ siputil.erl
@@ -1,44 +1,55 @@
 -module(siputil).
 -export([linefix/1, printvalue/1, concat_strings/1]).
 
-linefix([]) ->
-    [];
+linefix(In) ->
+    linefix2(In, []).
 
-linefix([13]) ->
-    [10];
+%% 13          -> 10
+%% 10          -> 10
+%% 13 10 13 10 -> 10 10
+%% 10 10       -> 10 10
+%% 13 13       -> 10 10
 
-linefix([10]) ->
-    [10];
+%% header-body separator, stop processing (leave body untouched)
+linefix2([13, 10, 13, 10 | T], Res) ->
+    L1 = [10, 10 | Res],
+    L = lists:foldl(fun(H, Acc) ->
+			    [H|Acc]
+		    end, L1, T),
+    lists:reverse(L);
+linefix2([10, 10 | T], Res) ->
+    L1 = [10, 10 | Res],
+    L = lists:foldl(fun(H, Acc) ->
+			    [H|Acc]
+		    end, L1, T),
+    lists:reverse(L);
+linefix2([13, 13 | T], Res) ->
+    L1 = [10, 10 | Res],
+    L = lists:foldl(fun(H, Acc) ->
+			    [H|Acc]
+		    end, L1, T),
+    lists:reverse(L);
 
-linefix([A]) ->
-    [A];
+
+linefix2([13, 10 | T], Res) ->
+    linefix2(T, [10 | Res]);
 
 
+linefix2([13 | T], Res)  ->
+    linefix2(T, [10 | Res]);
+
+linefix2([10 | T], Res) ->
+    linefix2(T, [10 | Res]);
 
 
-linefix([13, 10, 13, 10 | C]) ->
-    [10, 10 | C];
+linefix2([H | T], Res) ->
+    %% any char
+    linefix2(T, [H | Res]);
 
-linefix([10, 10 | C]) ->
-    [10, 10 | C];
+linefix2([], Res) ->
+    %% no input left, return
+    lists:reverse(Res).
 
-linefix([13, 13 | C]) ->
-    [10, 10 | C];
-
-
-
-
-linefix([13, 10 | C]) ->
-    [10 | linefix(C)];
-
-linefix([13, B | C]) ->
-    [10 | linefix([B | C])];
-
-linefix([A, 10 | C]) ->
-    [A | linefix([10 | C])];
-
-linefix([A, B | C]) ->
-    [A | linefix([B | C])].
 
 printvalue([]) ->
     [];
