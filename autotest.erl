4a133e1b7ce0fc7748e9bba94db4e55bcb068031
Fredrik Thulin <ft@it.su.se>
2004-12-23 15:06:43 +0000

    add module sipdst
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@772 e454d3dc-12ff-0310-9b8b-94264392205f

--- autotest.erl
+++ autotest.erl
@@ -43,7 +43,8 @@
 		       sippacket,
 		       sipserver,
 		       servertransaction,
-		       clienttransaction
+		       clienttransaction,
+		       sipdst
 		      ]).
 
 %%====================================================================
