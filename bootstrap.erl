12e4e758c3aa73b0866e8806544971ed659a6796
Fredrik Thulin <ft@it.su.se>
2005-01-12 18:34:59 +0000

    show progress through a series of io:format()'s
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@780 e454d3dc-12ff-0310-9b8b-94264392205f

--- bootstrap.erl
+++ bootstrap.erl
@@ -2,13 +2,42 @@
 -export([start/1]).
 
 start([AdminPassword]) ->
-    io:format("Bootstrapping Yxa on node ~p~n", [node()]),
-    ok = mnesia:create_schema([node()]),
+    io:format("Bootstrapping Yxa on node ~p :~n", [node()]),
+    ok = create_schema(node()),
     ok = mnesia:start(),
-    io:format("Creating tables on this Mnesia node (~p)~n", [node()]),
-    phone:create([node()]),
-    database_regexproute:create([node()]),
-    database_forward:create([node()]),
-    database_call:create([node()]),
-    io:format("Creating admin-user~n"),
-    phone:insert_user("admin", AdminPassword, [admin], []).
+
+    io:format("* Creating tables on this Mnesia node (~p)~n", [node()]),
+    init_db_module([phone,
+		    database_regexproute,
+		    database_forward,
+		    database_call
+		   ], node()),
+
+    io:format("* Updating any pre-existing table definitions~n"),
+    ok = table_update:update(),
+
+    io:format("* Creating admin-user~n"),
+    phone:insert_user("admin", AdminPassword, [admin], []),
+
+    ok = mnesia:stop(),
+    io:format("~nBootstrapping complete.~n~n").
+
+create_schema(Node) ->
+    case mnesia:create_schema([Node]) of
+	ok ->
+	    io:format("* Created Mnesia schema on node ~p~n", [Node]);
+	{error, {Node, {already_exists, Node}}} ->
+	    io:format("* Mnesia schema already existed on node ~p~n", [Node]);
+	E ->
+	    io:format("~nFailed creating Mnesia schema on node ~p :~n~p---~nExiting.",
+		      [Node, E]),
+	    erlang:halt(1)
+    end,
+    ok.
+
+init_db_module([H | T], Node) ->
+    io:format("  + Creating table '~p'~n", [H]),
+    H:create([Node]),
+    init_db_module(T, Node);
+init_db_module([], _Node) ->
+    ok.
