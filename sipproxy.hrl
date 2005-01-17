303eb1f5bdb7910482511fd91f8a42cc3dc2bb1c
Fredrik Thulin <ft@it.su.se>
2005-01-17 20:08:09 +0000

    add sp_response record
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@798 e454d3dc-12ff-0310-9b8b-94264392205f

--- sipproxy.hrl
+++ sipproxy.hrl
@@ -1,1 +1,19 @@
--record(sipproxy_action, {action, timeout, requri}).
+%%
+%% -------------------------------------------------------------------
+
+-record(sipproxy_action, {
+	  action, 
+	  timeout, 
+	  requri
+	 }).
+
+%% sipproxy response record. only used internally when processing responses,
+%% and stored in our 'response context' (targetlist that is).
+-record(sp_response, {
+	  status,	%% integer(), same as in record 'response'
+	  reason,	%% string(), same as in record 'response'
+	  header,	%% keylist record(), same as in record 'response'
+	  body,		%% binary(), same as in record 'response'
+	  created	%% true | false, was this a response record or not?
+	  }).
+
