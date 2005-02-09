67d0df9ce0dabae80af3841647a58eb5b9417008
Fredrik Thulin <ft@it.su.se>
2005-02-09 07:14:15 +0000

    database_regexproute updates - store address as string instead of as sipurl record
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@883 e454d3dc-12ff-0310-9b8b-94264392205f

--- table_update.erl
+++ table_update.erl
@@ -1,6 +1,6 @@
-%% this code updates older database tables - code commited before 
+%% this code updates older database tables - code commited before
 %% 25-10-2004
-%% 
+%%
 %%--------------------------------------------------------------------
 
 -module(table_update).
@@ -37,7 +37,7 @@
 %%====================================================================
 
 %%--------------------------------------------------------------------
-%% Function: 
+%% Function:
 %% Descrip.: update databases
 %% Returns : ok
 %%--------------------------------------------------------------------
@@ -45,99 +45,109 @@ update() ->
     logger:log(debug, "Checking if any mnesia tables needs updating"),
     phone(),
     regexproute(),
-    ok.   
+    ok.
 
 %%--------------------------------------------------------------------
-%% Function: 
+%% Function:
 %% Descrip.: phone record got two new fields, add dummy fields for old
 %%           existing database entries
-%% Returns : 
+%% Returns :
 %%--------------------------------------------------------------------
 phone() ->
     put(update, false),
     F = fun
 	    %% check for old record lacking callid and cseq field
-	    ({phone, Number, Flags, Class, Expire, Address, ReqUriStr}) -> 
+	    ({phone, Number, Flags, Class, Expire, Address, ReqUriStr}) ->
 		put(update, true),
 		#phone{
 		     number = Number,
-		     flags = Flags, 
-		     class = Class, 
-		     expire = Expire,	
+		     flags = Flags,
+		     class = Class,
+		     expire = Expire,
 		     address = Address,
-		     requristr = ReqUriStr, 
-		     callid = "", 
-		     cseq = 0    
-		    };
-	    %% debug related patch - to fix when new phone entries got improperly updated - 
-	    %% forgot setting CallId and CSeq field values
-	    ({phone, Number, Flags, Class, Expire, Address, ReqUriStr, undefined, undefined}) -> 
-		put(update, true),
-		#phone{
-		     number = Number,
-		     flags = Flags, 
-		     class = Class, 
-		     expire = Expire,	
-		     address = Address,
-		     requristr = ReqUriStr, 
-		     callid = "", 
+		     requristr = ReqUriStr,
+		     callid = "",
 		     cseq = 0
-		    };    
+		    };
+	    %% debug related patch - to fix when new phone entries got improperly updated -
+	    %% forgot setting CallId and CSeq field values
+	    ({phone, Number, Flags, Class, Expire, Address, ReqUriStr, undefined, undefined}) ->
+		put(update, true),
+		#phone{
+		     number = Number,
+		     flags = Flags,
+		     class = Class,
+		     expire = Expire,
+		     address = Address,
+		     requristr = ReqUriStr,
+		     callid = "",
+		     cseq = 0
+		    };
 	    %% nothing to update
-	    ({phone, _Number, _Flags, _Class, _Expire, _Address, _ReqUriStr, _CallId, _CSeq} = Phone) -> 
+	    ({phone, _Number, _Flags, _Class, _Expire, _Address, _ReqUriStr, _CallId, _CSeq} = Phone) ->
 		Phone
 	end,
     {atomic, ok} = mnesia:transform_table(phone, F, record_info(fields, phone)),
 
     case erase(update) of
-	true -> 
+	true ->
 	    logger:log(debug, "phone: updated");
 	false ->
 	    true
     end.
 
 %%--------------------------------------------------------------------
-%% Function: 
-%% Descrip.: update the sipurl record() in the regexproute
-%% Returns : 
+%% Function:
+%% Descrip.: update the sipurl record() in the regexproute, and store
+%%           it as a string instead of as a record.
+%% Returns :
 %%--------------------------------------------------------------------
 regexproute() ->
     put(update, false),
     F = fun
 	    %% check for old sipurl's lacking url_param field
-	    ({regexproute, 
-	      Regexp, 
-	      Flags, 
-	      Class, 
-	      Expire, 
-	      {
-		sipurl,
-		Proto, 
-		User,
-		Pass,
-		Host,
-		Port,
-		Param        
-	       }
+	    ({regexproute, Regexp, Flags,  Class,  Expire,
+	      {sipurl, Proto, User, Pass, Host, Port, Param}
 	     }) ->
 		put(update, true),
+		%% fixes so that url_param record is used
+		URL = sipurl:new([{proto, Proto}, {user, User}, {pass, Pass},
+				  {host, Host}, {port, Port}, {param, Param}]),
+		%% store as string instead of record, so that we don't have to do
+		%% conversions like this in the future when we modify records
+		URLstr = sipurl:print(URL),
 		#regexproute{
-		     regexp = Regexp, 
-		     flags = Flags, 
-		     class = Class, 
-		     expire = Expire, 
-		     %% fixes so that url_param record is used
-		     address = sipurl:new([{proto, Proto}, {user, User}, {pass, Pass},
-					   {host, Host}, {port, Port}, {param, Param}])
+		     regexp = Regexp,
+		     flags = Flags,
+		     class = Class,
+		     expire = Expire,
+		     address = URLstr
+		    };
+	    %% check for sipurl's with both param and url_param field
+	    ({regexproute, Regexp, Flags,  Class,  Expire,
+	      {sipurl, Proto, User, Pass, Host, Port, _Param, UrlParam}
+	     }) ->
+		put(update, true),
+		URL = sipurl:new([{proto, Proto}, {user, User}, {pass, Pass},
+				  {host, Host}, {port, Port}, {param, UrlParam}]),
+		%% store as string instead of record, so that we don't have to do
+		%% conversions like this in the future when we modify records
+		URLstr = sipurl:print(URL),
+		#regexproute{
+		     regexp = Regexp,
+		     flags = Flags,
+		     class = Class,
+		     expire = Expire,
+		     address = URLstr
 		    };
 	    %% nothing to update
-	    (RegExpRoute)  ->
+	    (RegExpRoute) when is_record(RegExpRoute, regexproute) ->
 		RegExpRoute
 	end,
     {atomic, ok} = mnesia:transform_table(regexproute, F, record_info(fields, regexproute)),
 
     case erase(update) of
-	true -> 
+	true ->
 	    logger:log(debug, "regexproute: updated");
 	false ->
 	    true
@@ -148,9 +158,9 @@ regexproute() ->
 %%====================================================================
 
 %%--------------------------------------------------------------------
-%% Function: 
-%% Descrip.: 
-%% Returns : 
+%% Function:
+%% Descrip.:
+%% Returns :
 %%--------------------------------------------------------------------
 
 %%====================================================================
