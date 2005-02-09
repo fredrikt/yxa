374543fbc264b9f4bd4c1b6f8e065faca721ae61
Fredrik Thulin <ft@it.su.se>
2005-02-09 06:44:12 +0000

    add comments
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@864 e454d3dc-12ff-0310-9b8b-94264392205f

--- database_regexproute.hrl
+++ database_regexproute.hrl
@@ -1,10 +1,11 @@
-%%
+%% Regexproute allows you to route requests based on regular
+%% expression matching in incomingproxy.
 %% -------------------------------------------------------------------
 
 -record(regexproute, {
-	  regexp, 
-	  flags, 
-	  class, 
-	  expire, 
-	  address
+	  regexp,	% string(), the regular expression
+	  flags,	% list() of {Key, Value}
+	  class,	% atom(), permanent | ...
+	  expire,	% integer() | never
+	  address	% string(), the target address. must be parseable by sipurl:parse().
 	 }).
