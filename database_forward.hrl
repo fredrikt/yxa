af5f36ba9e2f40c9356bd92810b2dda8b82e14d2
Fredrik Thulin <ft@it.su.se>
2005-03-15 15:08:44 +0000

    add comments
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@897 e454d3dc-12ff-0310-9b8b-94264392205f

--- database_forward.hrl
+++ database_forward.hrl
@@ -2,8 +2,9 @@
 %% -------------------------------------------------------------------
 
 -record(forward, {
-	  number, 
-	  forwards, 
-	  timeout, 
-	  localring
+	  number,	%% number (or SIP user) that has a forward
+	  forwards,	%% list() of sipurl record()
+	  timeout,	%% integer(), seconds to try these forwards
+	  localring	%% true | false, ring non-forward entrys at
+	  		%% the same time or not?
 	 }).
