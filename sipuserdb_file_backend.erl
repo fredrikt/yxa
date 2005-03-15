dd6af448b2c69cfc1b67868fa9b73cf7dcce1b36
Fredrik Thulin <ft@it.su.se>
2005-03-15 15:04:41 +0000

    Make it possible to list addresses as a part of the user declaration (while still keeping the old method of listing '{address, ...}' separately. Add test cases and update some documentation.
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@895 e454d3dc-12ff-0310-9b8b-94264392205f

--- sipuserdb_file_backend.erl
+++ sipuserdb_file_backend.erl
@@ -32,7 +32,9 @@
 	 handle_cast/2,
 	 handle_info/2,
 	 terminate/2,
-	 code_change/3
+	 code_change/3,
+
+	 test/0
 	]).
 
 %%--------------------------------------------------------------------
@@ -48,11 +50,11 @@
 %% Records
 %%--------------------------------------------------------------------
 -record(state, {
-	  fn,
-	  file_mtime,
-	  last_fail,
-	  userlist,
-	  addresslist
+	  fn,		%% string(), filename
+	  file_mtime,	%% integer(), mtime of fn
+	  last_fail,	%% integer(), timestamp when we last warned about failure to interpret fn
+	  userlist,	%% list() of user record()
+	  addresslist	%% list() of address record()
 	 }).
 
 %%--------------------------------------------------------------------
@@ -237,16 +239,17 @@ terminate(Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-%%--------------------------------------------------------------------
-%%% Internal functions
-%%--------------------------------------------------------------------
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
 
 %%--------------------------------------------------------------------
 %% Function: read_userdb(State, MTime, Caller)
 %%           State  = state record()
 %%           MTime  = integer(), file modification time
 %%           Caller = atom(), who is calling us (init | cast | info)
-%% Descrip.: (Re-)load the user database. 
+%% Descrip.: (Re-)load the user database.
 %% Returns : State           |
 %%           {error, Reason}
 %%--------------------------------------------------------------------
@@ -339,10 +342,12 @@ get_mtime(Fn) ->
 %%           Reason   = string()
 %%--------------------------------------------------------------------
 parse_db([TermList]) ->
-    parse_term(TermList, [], []).
+    {ok, U, A} = parse_term(TermList, [], []),
+    verify_consistency(U, A),
+    {ok, U, A}.
 
 %%--------------------------------------------------------------------
-%% Function: parse_term(In, UserList, AddrList) 
+%% Function: parse_term(In, UserList, AddrList)
 %%           In       = list() of {user, Params} | {address, Params}
 %%                      record()
 %%           UserList = list() of user record()
@@ -357,9 +362,17 @@ parse_db([TermList]) ->
 parse_term([], UserList, AddrList) ->
     {ok, lists:reverse(UserList), lists:reverse(AddrList)};
 parse_term([{user, Params} | T], UserList, AddrList) ->
-    case parse_user(Params, #user{}) of
-	R when is_record(R, user) ->
+    case parse_user(Params, #user{}, []) of
+	{ok, R, []} when is_record(R, user) ->
 	    parse_term(T, [R | UserList], AddrList);
+	{ok, R, Addrs} when is_record(R, user), is_list(Addrs) ->
+	    case parse_addresses(R#user.name, Addrs, AddrList) of
+		L when is_list(L) ->
+		    %% Parse addresses listed together with user
+		    parse_term(T, [R | UserList], lists:reverse(L));
+		E ->
+		    E
+	    end;
 	E ->
 	    E
     end;
@@ -371,31 +384,55 @@ parse_term([{address, Params} | T], UserList, AddrList) ->
 	    E
     end;
 parse_term([H | _T], _UserList, _AddrList) ->
-    {error, io_lib:format("sipuserdb_file: Unknown data : ~p", [H])}.
+    E = io_lib:format("sipuserdb_file: Unknown data : ~p", [H]),
+    {error, lists:flatten(E)}.
+
+
+%% part of parse_term/3, parse addresses listed together with user
+parse_addresses(Username, [H | T], Res) ->
+    case parse_address([{address, H}], #address{user=Username}) of
+	A when is_record(A, address) ->
+	    parse_addresses(Username, T, [A | Res]);
+	E ->
+	    E
+    end;
+parse_addresses(_Username, [], Res) ->
+    lists:reverse(Res).
 
 %%--------------------------------------------------------------------
-%% Function: parse_user(Params, U)
+%% Function: parse_user(Params, U, Addrs)
 %%           Params = list() of {Key, Value} tuple()
 %%           U      = user record()
+%%           Addrs  = list() of string() - just accumulator
 %% Descrip.: Parse a user entry.
-%% Returns : User            |
+%% Returns : {ok, User, AddrList} |
 %%           {error, Reason}
-%%           User   = user record()
-%%           Reason = string()
+%%           User     = user record()
+%%           AddrList = list() of string()
+%%           Reason   = string()
 %%--------------------------------------------------------------------
-parse_user([], U) when is_record(U, user) ->
-    U;
-parse_user([{name, V} | T], U) when is_record(U, user) ->
-    parse_user(T, U#user{name=V});
-parse_user([{password, V} | T], U) when is_record(U, user) ->
-    parse_user(T, U#user{password=V});
-parse_user([{classes, V} | T], U) when is_record(U, user) ->
-    parse_user(T, U#user{classes=V});
-parse_user([{forward, V} | T], U) when is_record(U, user) ->
-    parse_user(T, U#user{forward=V});
-parse_user([H | _T], U) when is_record(U, user) ->
+parse_user([], U, Addrs) when is_record(U, user), U#user.name == undefined ->
+    E = io_lib:format("user record incomplete (no user, raw user ~p, addresses ~p)", [U, Addrs]),
+    {error, lists:flatten(E)};
+parse_user([], U, Addrs) when is_record(U, user) ->
+    {ok, U, Addrs};
+
+parse_user([{name, V} | T], U, Addrs) when is_record(U, user), is_list(V) ->
+    parse_user(T, U#user{name=V}, Addrs);
+parse_user([{password, V} | T], U, Addrs) when is_record(U, user), is_list(V) ->
+    parse_user(T, U#user{password=V}, Addrs);
+parse_user([{classes, V} | T], U, Addrs) when is_record(U, user), is_list(V) ->
+    parse_user(T, U#user{classes=V}, Addrs);
+parse_user([{forward, V} | _T], U, _Addrs) when is_record(U, user), is_list(V) ->
+    E = io_lib:format("forward not implemented in sipuserdb_file yet (name ~p) : ~p",
+		      [U#user.name, {forward, V}]),
+    {error, lists:flatten(E)};
+parse_user([{addresses, V} | T], U, Addrs) when is_record(U, user), is_list(V) ->
+    parse_user(T, U, Addrs ++ V);
+parse_user([H | _T], U, _Addrs) when is_record(U, user) ->
     E = io_lib:format("bad data in user record (name ~p) : ~p", [U#user.name, H]),
-    {error, E}.
+    {error, lists:flatten(E)}.
+
 
 %%--------------------------------------------------------------------
 %% Function: parse_address(Params, A)
@@ -407,12 +444,275 @@ parse_user([H | _T], U) when is_record(U, user) ->
 %%           Address = address record()
 %%           Reason  = string()
 %%--------------------------------------------------------------------
-parse_address([], A) when is_record(A, address)->
+parse_address([], A) when is_record(A, address), A#address.user == undefined ->
+    E = io_lib:format("address record incomplete (no user, address ~p)", [A#address.address]),
+    {error, E};
+parse_address([], A) when is_record(A, address), A#address.address == undefined ->
+    E = io_lib:format("address record incomplete (no address, user ~p)", [A#address.user]),
+    {error, lists:flatten(E)};
+parse_address([], A) when is_record(A, address) ->
     A;
-parse_address([{user, V} | T], A) when is_record(A, address) ->
+
+parse_address([{user, V} | T], A) when is_record(A, address), is_list(V) ->
     parse_address(T, A#address{user=V});
-parse_address([{address, V} | T], A) when is_record(A, address) ->
-    parse_address(T, A#address{address=V});
+parse_address([{address, V} | T], A) when is_record(A, address), is_list(V) ->
+    case sipurl:parse(V) of
+	URL when is_record(URL, sipurl) ->
+	    parse_address(T, A#address{address=V, url=URL});
+	_ ->
+	    E = io_lib:format("unparsable URL in address record (user ~p) : ~p", [A#address.user, V]),
+	    {error, lists:flatten(E)}
+    end;
 parse_address([H | _T], A) when is_record(A, address) ->
     E = io_lib:format("bad data in address record (user ~p) : ~p", [A#address.user, H]),
-    {error, E}.
+    {error, lists:flatten(E)}.
+
+
+%%--------------------------------------------------------------------
+%% Function: verify_consistency(Users, Addresses)
+%%           Users    = list() of user record()
+%%           Adresses = list() of address record()
+%% Descrip.: Log warnings if there are any users that has no addresses
+%%           or any addresses that have no users.
+%% Returns : ok
+%%--------------------------------------------------------------------
+verify_consistency(Users, Addresses) ->
+    {ok, NoAddressUsers, NoUserAddresses} = verify_consistency2(Users, Addresses),
+    case NoAddressUsers of
+	[] -> ok;
+	_ ->
+	    logger:log(normal, "sipuserdb_file: Warning: The following user(s) have no address(es) : ~p",
+		       [NoAddressUsers])
+    end,
+    case NoUserAddresses of
+	[] -> ok;
+	_ ->
+	    logger:log(normal, "sipuserdb_file: Warning: The following address(es) have no user(s) : ~p",
+		       [NoUserAddresses])
+    end,
+    ok.
+
+%% verify_consistency2/2 - part of verify_consistency/2
+%% Returns : {ok, NoAddressUsers, NoUserAddresses}
+verify_consistency2(Users, Addresses) ->	     
+    NoAddressUsers = get_no_address_users(Users, Addresses, []),
+    NoUserAddresses = get_no_user_addresses(Addresses, Users, []),
+    {ok, NoAddressUsers, NoUserAddresses}.
+
+%% get_no_address_users/3 - part of verify_consistency2/2
+%% Returns : NoAddressUsers = list() of string() - users who have no addresses
+get_no_address_users([H | T], Addresses, Res) when is_record(H, user) ->
+    case get_no_address_users2(H#user.name, Addresses) of
+	true ->
+	    %% there was at least one address for this user
+	    get_no_address_users(T, Addresses, Res);
+	false ->
+	    %% there was no addresses for this user
+	    get_no_address_users(T, Addresses, [H#user.name | Res])
+    end;
+get_no_address_users([], _Addresses, Res) ->
+    lists:reverse(Res).
+
+%% get_no_address_users2/2 - part of get_no_address_users/3
+get_no_address_users2(Username, [#address{user=Username} | _T]) ->
+    true;
+get_no_address_users2(Username, [_H | T]) ->
+    get_no_address_users2(Username, T);
+get_no_address_users2(_Username, []) ->
+    false.
+
+%% get_no_user_addresses/3 - part of verify_consistency2/2
+%% Returns : NoUserAddresses = list() of string() - addresses who have no user
+get_no_user_addresses([H | T], Users, Res) when is_record(H, address) ->
+    case get_no_user_addresses2(H#address.user, Users) of
+	true ->
+	    %% there was at least one user for this address
+	    get_no_user_addresses(T, Users, Res);
+	false ->
+	    %% there was no user for this addresses
+	    get_no_user_addresses(T, Users, [H#address.address | Res])
+    end;
+get_no_user_addresses([], _Users, Res) ->
+    lists:reverse(Res).
+
+%% get_no_user_addresses2/2 - part of get_no_user_addresses/3
+get_no_user_addresses2(Username, [#user{name=Username} | _T]) ->
+    true;
+get_no_user_addresses2(Username, [_H | T]) ->
+    get_no_user_addresses2(Username, T);
+get_no_user_addresses2(_Username, []) ->
+    false.
+
+
+%%====================================================================
+%% Test functions
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% Function: test()
+%% Descrip.: autotest callback
+%% Returns : ok | throw()
+%%--------------------------------------------------------------------
+test() ->
+
+    %% test parse_address/1
+    %%--------------------------------------------------------------------
+    ParseAddress_URL1 = sipurl:parse("sip:ft@example.org"),
+
+    io:format("test: parse_address/2 - 1~n"),
+    %% valid entry
+    #address{user    = "test",
+	     address = "sip:ft@example.org",
+	     url     = ParseAddress_URL1} =
+	parse_address([{user, "test"}, {address, "sip:ft@example.org"}], #address{}),
+
+    io:format("test: parse_address/2 - 2~n"),
+    %% unparsable URL
+    {error, "unparsable URL in address record " ++ _} =
+	parse_address([{address, "unsupported:ft@example.org"}], #address{}),
+
+    io:format("test: parse_address/2 - 3~n"),
+    %% no user
+    {error, "address record incomplete (no user, " ++ _} =
+	parse_address([{address, "sip:ft@example.org"}], #address{}),
+
+    io:format("test: parse_address/2 - 4~n"),
+    %% no address
+    {error, "address record incomplete (no address, " ++ _} =
+	parse_address([{user, "test"}], #address{}),
+
+    io:format("test: parse_address/2 - 5~n"),
+    %% non-list user
+    {error, "bad data in address record " ++ _} = parse_address([{user, none}], #address{}),
+
+    io:format("test: parse_address/2 - 6~n"),
+    %% non-list address
+    {error, "bad data in address record " ++ _} = parse_address([{address, none}], #address{}),
+
+    io:format("test: parse_address/2 - 7~n"),
+    %% unknown parameter
+    {error, "bad data in address record " ++ _} = parse_address([{true, "x"}], #address{}),
+
+
+    %% test parse_user/3
+    %%--------------------------------------------------------------------
+    io:format("test: parse_user/3 - 1~n"),
+    %% minimalistic case
+    {ok, #user{name="foo"}, []} = parse_user([{name, "foo"}], #user{}, []),
+
+    io:format("test: parse_user/3 - 2~n"),
+    %% all settings possible
+    {ok, #user{name     = "foo",
+	       password = "secret",
+	       classes  = [none],
+	       forward  = undefined
+	      }, ["test"]} =
+	parse_user([{name, "foo"}, {password, "secret"}, {classes, [none]}, {addresses, ["test"]}], #user{}, []),
+
+    io:format("test: parse_user/3 - 3~n"),
+    %% test not-implemented forward
+    {error, "forward not implemented in sipuserdb_file yet" ++ _} = parse_user([{forward, "foo"}], #user{}, []),
+
+    io:format("test: parse_user/3 - 4~n"),
+    %% non-list username
+    {error, "bad data in user record" ++ _} = parse_user([{name, none}], #user{}, []),
+
+    io:format("test: parse_user/3 - 5~n"),
+    %% without username
+    {error, "user record incomplete (no user, " ++ _} = parse_user([{password, "secret"}], #user{}, []),
+
+
+    %% test parse_term/3
+    %%--------------------------------------------------------------------
+    ParseTermUserL1 = [#user{name = "t1"},
+		       #user{name = "t2"}],
+    ParseTermAddrL1 = [#address{user = "t1", address = "sip:1@example.net", url = sipurl:parse("sip:1@example.net")},
+		       #address{user = "t2", address = "sip:2@example.net", url = sipurl:parse("sip:2@example.net")}],
+
+    ParseTermAddrL2 = [#address{user = "t1", address = "sip:1.1@example.net", url = sipurl:parse("sip:1.1@example.net")},
+		       #address{user = "t1", address = "sip:1.2@example.net", url = sipurl:parse("sip:1.2@example.net")},
+		       #address{user = "t2", address = "sip:2.1@example.net", url = sipurl:parse("sip:2.1@example.net")},
+		       #address{user = "t2", address = "sip:2.2@example.net", url = sipurl:parse("sip:2.2@example.net")}],
+
+    io:format("test: parse_term/3 - 1~n"),
+    %% uncomplicated case
+    {ok, ParseTermUserL1, ParseTermAddrL1} = parse_term([{user, [{name, "t1"}]},
+							 {address, [{user, "t1"},
+								    {address, "sip:1@example.net"}]},
+							 
+							 {user, [{name, "t2"}]},
+							 {address, [{user, "t2"},
+								    {address, "sip:2@example.net"}]}
+							 ], [], []),
+
+    io:format("test: parse_term/3 - 2~n"),
+    %% addresses grouped with user
+    {ok, ParseTermUserL1, ParseTermAddrL2} = parse_term([{user, [{name, "t1"}, 
+								 {addresses, ["sip:1.1@example.net",
+									      "sip:1.2@example.net"]}]},
+							 {user, [{name, "t2"},
+								 {addresses, ["sip:2.1@example.net",
+									      "sip:2.2@example.net"]}]}
+							], [], []),
+
+    io:format("test: parse_term/3 - 3~n"),
+    %% unknown term
+    {error, "sipuserdb_file: Unknown data :" ++ _} = parse_term([{none, []}], [], []),
+
+    io:format("test: parse_term/3 - 4~n"),
+    %% user without name
+    {error, "bad data in user record" ++ _} = parse_term([{user, [{password, secret}]}], [], []),
+
+    io:format("test: parse_term/3 - 5~n"),
+    %% invalid address listed with user
+    {error, "unparsable URL in address record " ++ _} =
+	parse_term([{user, [{name, "t1"},
+			    {addresses, ["unsupported:ft@example.org"]}
+			   ]}], [], []),
+
+    io:format("test: parse_term/3 - 6~n"),
+    %% invalid address
+    {error, "unparsable URL in address record " ++ _} =
+	parse_term([{address, [{user, "t1"},
+			       {address, "unsupported:ft@example.org"}
+			      ]}], [], []),
+
+
+    %% test verify_consistency2(Users, Addresses)
+    %%--------------------------------------------------------------------
+    io:format("test: verify_consistency2/2 - 1~n"),
+    %% no warnings
+    {ok, VC_U1, VC_A1} = parse_term([{user, [{name, "test1"},
+					     {addresses, ["sip:test@example.org"]}
+					    ]},
+				     {user, [{name, "test2"},
+					     {addresses, ["sip:test@example.org"]}
+					    ]}
+				     ], [], []),
+    {ok, [], []} = verify_consistency2(VC_U1, VC_A1),
+
+    io:format("test: verify_consistency2/2 - 2~n"),
+    %% one address warning
+    {ok, VC_U2, VC_A2} = parse_term([{user, [{name, "test1"},
+					     {addresses, ["sip:test@example.org"]}
+					    ]},
+				     {user, [{name, "test2"},
+					     {addresses, ["sip:test@example.org"]}
+					    ]},
+				     {address, [{user, "x"},
+						{address, "sip:x@example.org"}]}
+				     ], [], []),
+    {ok, [], ["sip:x@example.org"]} = verify_consistency2(VC_U2, VC_A2),
+
+    io:format("test: verify_consistency2/2 - 3~n"),
+    %% one user warning
+    {ok, VC_U3, VC_A3} = parse_term([{user, [{name, "test1"}]},
+				     {user, [{name, "test2"},
+					     {addresses, ["sip:test@example.org"]}
+					    ]}
+				     ], [], []),
+    {ok, ["test1"], []} = verify_consistency2(VC_U3, VC_A3),
+
+
+    
+    ok.
