2d9b829c48e9fd7b64d471b9343710bc73e530d0
Fredrik Thulin <ft@it.su.se>
2005-03-15 17:37:58 +0000

    add comments and add element url to record address
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@922 e454d3dc-12ff-0310-9b8b-94264392205f

--- sipuserdb_file.hrl
+++ sipuserdb_file.hrl
@@ -1,10 +1,12 @@
 -record(user, {
-	  name,
-	  password,
-	  classes,
-	  forward
+	  name,		%% string(), username
+	  password,	%% string(), password
+	  classes,	%% list() of atom()
+	  forward	%% list() of sipproxy_forward record() | undefined - currently
+	  		%% not supported by sipuserdb_file, so always 'undefined'
 	 }).
 -record(address, {
-	  user,
-	  address
+	  user,		%% string(), username
+	  address,	%% string(), address
+	  url		%% string(), address parsed with sipurl:parse()
 	 }).
