83b4cff3a1ff138b51a10cf86d2aa7da6d83475d
Fredrik Thulin <ft@it.su.se>
2005-03-23 07:46:34 +0000

    various cleanups, no real change in functionality
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@929 e454d3dc-12ff-0310-9b8b-94264392205f

--- clienttransaction.erl
+++ clienttransaction.erl
@@ -31,20 +31,35 @@
 -module(clienttransaction).
 
 -behaviour(gen_server).
-%%--------------------------------------------------------------------
-%% Include files
-%%--------------------------------------------------------------------
 
 %%--------------------------------------------------------------------
 %% External exports
+%%--------------------------------------------------------------------
 -export([
 	 start_link/6,
 	 test/0
 	]).
 
-%% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+%%--------------------------------------------------------------------
+%% Internal exports - gen_server callbacks
+%%--------------------------------------------------------------------
+-export([init/1,
+	 handle_call/3,
+	 handle_cast/2,
+	 handle_info/2,
+	 terminate/2,
+	 code_change/3
+	]).
 
+%%--------------------------------------------------------------------
+%% Include files
+%%--------------------------------------------------------------------
+-include("sipsocket.hrl").
+-include("siprecords.hrl").
+
+%%--------------------------------------------------------------------
+%% Records
+%%--------------------------------------------------------------------
 -record(state, {
 	  branch,	%% string(), the Via branch paramter for this client transaction
 	  socket_in,	%% sipsocket record(), the socket the original request was received on
@@ -72,6 +87,7 @@
 %%====================================================================
 %% External functions
 %%====================================================================
+
 %%--------------------------------------------------------------------
 %% Function: start_link(Request, SocketIn, Dst, Branch, Timeout,
 %%                      ReportTo)
@@ -208,7 +224,7 @@ handle_call(Request, From, State) ->
 %%--------------------------------------------------------------------
 handle_cast({sipmessage, Response, Origin, _LogStr}, State) when is_record(Response, response),
 								 is_record(Origin, siporigin) ->
-    %% We have received a response to one of our requests.
+    %% We have received a response to our request.
     LogTag = State#state.logtag,
     LogLevel = if
 		   Response#response.status >= 200 -> normal;
@@ -307,7 +323,7 @@ handle_info({siptimer, TRef, TDesc}, State) ->
 		    Unknown ->
 			logger:log(error, "~s: Client transaction: process_timer() returned unknown result :~n~p",
 				   [LogTag, Unknown]),
-			State
+			erlang:error("clienttransaction: process_timer/2 returned unknown data")
 		end
 	end,
     check_quit({noreply, NewState});
@@ -318,8 +334,7 @@ handle_info({siptimer, TRef, TDesc}, State) ->
 %%           returned, so the caller (the transaction_layer) is not
 %%           blocked. Invoke initiate_request/1 to start trying to
 %%           send our request to it's destination.
-%% Returns : {noreply, State}          |
-%%           {stop, Reason, State}            (terminate/2 is called)
+%% Returns : {noreply, State}
 %%--------------------------------------------------------------------
 handle_info(timeout, #state{initialized=no}=State) ->
     {noreply, initiate_request(State#state{initialized=yes})};
@@ -350,7 +365,7 @@ handle_info({'EXIT', Pid, Reason}, #state{parent=Parent}=State) when Pid == Pare
 				   [LogTag, Parent, State#state.sipstate, State#state.cancelled]),
 			State
 		end,
-    
+
     %% clear report_to if it was the same as our parent (most oftenly is)
     NewState2 = case State#state.report_to of
 		   Parent ->
@@ -398,9 +413,9 @@ terminate(Reason, State) ->
 
 
 %%--------------------------------------------------------------------
-%% Func: code_change/3
-%% Purpose: Convert process state when code is changed
-%% Returns: {ok, NewState}
+%% Function: code_change(OldVsn, State, Extra)
+%% Descrip.: Convert process state when code is changed
+%% Returns : {ok, NewState}
 %%--------------------------------------------------------------------
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
@@ -501,7 +516,7 @@ process_timer2({resendrequest}, Timer, State) when is_record(State, state) ->
     {LogTag, SipState} = {State#state.logtag, State#state.sipstate},
     Request = State#state.request,
     {Method, URI} = {Request#request.method, Request#request.uri},
-    case lists:member(SipState, [trying, calling]) of
+    case (SipState == trying) or (SipState == calling) of
 	true ->
 	    logger:log(debug, "~s: Resend after ~s (request -> ~s): ~s",
 		       [LogTag, siptimer:timeout2str(Timeout), sipurl:print(URI), Method]),
@@ -512,7 +527,7 @@ process_timer2({resendrequest}, Timer, State) when is_record(State, state) ->
 	    NewTimerList = siptimer:revive_timer(Timer, NewTimeout, State#state.timerlist),
 	    NewState1 = State#state{timerlist=NewTimerList},
 	    NewState1;
-	_ ->
+	false ->
 	    logger:log(debug, "~s: Ignoring resendrequest of ~s ~s when in state ~p",
 		       [LogTag, Method, sipurl:print(URI), SipState]),
 	    State
@@ -540,9 +555,12 @@ process_timer2({resendrequest_timeout}, Timer, State) when is_record(State, stat
 	calling ->
 	    fake_request_timeout(State);
 	proceeding when Method == "INVITE" ->
-	    logger:log(debug, "~s: Sending of original request (INVITE ~s) timed out in state " ++
-		       "'proceeding' - cancel original request",
-		       [LogTag, sipurl:print(URI)]),
+	    %% XXX Should we ever end up here? RFC3261 17.1.1 does not specify any action for
+	    %% Timer B for INVITE client transactions except in the 'calling' state.
+	    logger:log(debug, "~s: Sending of request 'INVITE ~s' timed out in state 'proceeding' - "
+		       "cancel myself", [LogTag, sipurl:print(URI)]),
+	    %% XXX should we generate a '408 Request Timeout' for our parent (transaction user),
+	    %% similar to what we do when our {invite_timeout} timer fires?
 	    cancel_request(State);
 	proceeding ->
 	    fake_request_timeout(State);
@@ -644,9 +662,9 @@ process_received_response(Response, State) when is_record(State, state), is_reco
 		%% Cancel/reset INVITE request expire timer when receiving provisional responses
 		update_invite_expire(Status, NewState1);
 	    "INVITE" when Status >= 300, Status =< 699  ->
-		%% ACK any 3xx, 4xx, 5xx and 6xx responses if they are responses to an INVITE
+		%% ACK any 3/4/5/6xx responses if they are responses to an INVITE
 		%% and we are in either 'calling' or 'proceeding' state
-		ack_non_1xx_or_2xx_response_to_invite(Response, NewState1);
+		ack_response_to_invite(Response, NewState1);
 	    _ ->
 		NewState1
 	end,
@@ -676,14 +694,14 @@ update_transaction_state(Response, NewSipState, BranchAction, State) when is_rec
     OldSipState = State#state.sipstate,
     case NewSipState of
 	OldSipState ->
-	    logger:log(debug, "~s: Evaluated ~p ~s (in response to ~s ~s). Remaining in state '~p', action '~p'.",
+	    logger:log(debug, "~s: Evaluated '~p ~s' (in response to ~s ~s). Remaining in state '~p', action '~p'.",
 		       [LogTag, Status, Reason, Method, sipurl:print(URI), OldSipState, BranchAction]),
 	    %% Update response so that we report exactly this response to our parent and not any previous
 	    %% (for example, report a previous 183 instead of this new one)
 	    NewState = State#state{response=Response},
 	    NewState;
 	_ ->
-	    logger:log(debug, "~s: Evaluated ~p ~s (in response to ~s ~s). Changing state from '~p' "
+	    logger:log(debug, "~s: Evaluated '~p ~s' (in response to ~s ~s). Changing state from '~p' "
 		       "to '~p', action '~p'.", [LogTag, Status, Reason, Method, sipurl:print(URI),
 						 OldSipState, NewSipState, BranchAction]),
 	    NewState = State#state{sipstate=NewSipState, response=Response},
@@ -711,7 +729,7 @@ act_on_new_sipstate(OldSipState, NewSipState, BranchAction, State)
     %% State has not changed
     {State, BranchAction};
 act_on_new_sipstate(OldSipState, NewSipState, BranchAction, State)
-  when is_atom(OldSipState), is_atom(NewSipState),  is_atom(BranchAction),
+  when is_atom(OldSipState), is_atom(NewSipState), is_atom(BranchAction),
        BranchAction == ignore; BranchAction == tell_parent,
        is_record(State, state) ->
     %% State has changed
@@ -932,19 +950,19 @@ received_response_state_machine(Method, Status, State) when Status >= 200, Statu
 
 
 received_response_state_machine(Method, Status, trying) when Status >= 300, Status =< 599 ->
-    logger:log(debug, "UAC decision: Received 3xx, 4xx or 5xx response ~p to ~s when in state 'trying', " ++
+    logger:log(debug, "UAC decision: Received 3/4/5xx response ~p to ~s when in state 'trying', " ++
 	       "entering state 'completed' and telling parent", [Status, Method]),
     {tell_parent, completed};
 received_response_state_machine("INVITE", Status, calling) when Status >= 300, Status =< 599 ->
-    logger:log(debug, "UAC decision: Received 3xx, 4xx or 5xx response ~p to INVITE when in state 'calling', " ++
+    logger:log(debug, "UAC decision: Received 3/4/5xx response ~p to INVITE when in state 'calling', " ++
 	       "entering state 'completed' and telling parent", [Status]),
     {tell_parent, completed};
 received_response_state_machine(Method, Status, proceeding) when Status >= 300, Status =< 599 ->
-    logger:log(debug, "UAC decision: Received 3xx, 4xx or 5xx response ~p to ~s when in state 'proceeding', " ++
+    logger:log(debug, "UAC decision: Received 3/4/5xx response ~p to ~s when in state 'proceeding', " ++
 	       "entering state 'completed' and telling parent", [Status, Method]),
     {tell_parent, completed};
 received_response_state_machine(Method, Status, State) when Status >= 300, Status =< 599 ->
-    logger:log(debug, "UAC decision: Received 3xx, 4xx or 5xx response ~p to ~s when in state '~p', " ++
+    logger:log(debug, "UAC decision: Received 3/4/5xx response ~p to ~s when in state '~p', " ++
 	       "ignoring", [Status, Method, State]),
     {ignore, State};
 
@@ -1096,7 +1114,7 @@ fake_request_response(Status, Reason, State) when is_record(State, state) ->
 %%           Status = integer(), SIP status code
 %%           Reason = string(), SIP reason phrase
 %%           State  = state record()
-%% Descrip.: Fake receiving an '408 Request Terminated' response. See
+%% Descrip.: Fake receiving an '408 Request Timeout' response. See
 %%           comments for fake_request_response/3 above for more info.
 %% Returns : NewState = state record()
 %%--------------------------------------------------------------------
@@ -1300,21 +1318,20 @@ update_invite_expire(Status, State) when is_record(State, state), Status >= 101,
     end.
 
 %%--------------------------------------------------------------------
-%% Function: ack_non_1xx_or_2xx_response_to_invite(Method, Response,
-%%                                                 State)
+%% Function: ack_response_to_invite(Method, Response, State)
 %%           Method   = string()
 %%           Response = response record()
 %%           State    = state record()
 %% Descrip.: Send an ACK if this is an INVITE transaction and the
-%%           response received was a 3xx, 4xx, 5xx or 6xx.
+%%           response received was a 3/4/5/6xx.
 %% Returns : NewState = state record()
 %%--------------------------------------------------------------------
-ack_non_1xx_or_2xx_response_to_invite(#response{status=Status}=Response, State)
+ack_response_to_invite(#response{status=Status}=Response, State)
   when is_record(State, state), Status >= 300, Status =< 699 ->
     LogTag = State#state.logtag,
     Request = State#state.request,
     URI = Request#request.uri,
-    logger:log(debug, "~s: ACK-ing 3xx, 4xx, 5xx or 6xx response ~p ~s to INVITE ~s (in state '~p')",
+    logger:log(debug, "~s: ACK-ing 3/4/5/6xx response '~p ~s' to INVITE ~s (in state '~p')",
 	       [LogTag, Status, Response#response.reason, sipurl:print(URI), State#state.sipstate]),
     generate_ack(Response, State),
     State.
