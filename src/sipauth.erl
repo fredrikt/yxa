8a680deae9a149be794da74d22769c213713a151
Fredrik Thulin <ft@it.su.se>
2005-10-05 05:33:03 +0000

    get_user_verified2: Disable GSSAPI function clause since it has been broken for ages, and now does not even match the arguments to the function.
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@1213 e454d3dc-12ff-0310-9b8b-94264392205f

--- src/sipauth.erl
+++ src/sipauth.erl
@@ -236,13 +236,13 @@ get_user_verified_yxa_peer2(_Header, _Method, [], _Realm, LastRes) ->
     logger:log(debug, "Auth: No more auth headers matching my realm, returning last result : ~p", [LastRes]),
     LastRes.
 
-get_user_verified2(_Method, ["GSSAPI" ++ _R] = Authheader, _Header) ->
-    Authorization = sipheader:auth(Authheader),
-    Info = dict:fetch("info", Authorization),
-    {_Response, Username} = gssapi:request(Info),
-    %% XXX this is definately broken! What does gssapi:request() return anyways?
-    Username,
-    erlang:fault({error, "GSSAPI code broken and not yet fixed"});
+%%get_user_verified2(_Method, ["GSSAPI" ++ _R] = Authheader, _Header) ->
+%%    Authorization = sipheader:auth(Authheader),
+%%    Info = dict:fetch("info", Authorization),
+%%    {_Response, Username} = gssapi:request(Info),
+%%    %% XXX this is definately broken! What does gssapi:request() return anyways?
+%%    Username,
+%%    erlang:fault({error, "GSSAPI code broken and not yet fixed"});
 
 %%--------------------------------------------------------------------
 %% Function: get_user_verified2(Method, AuthDicts, Header)
