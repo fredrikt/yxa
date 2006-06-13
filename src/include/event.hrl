9b8e2d44500731b79ddc1630dad3084fb0e25c76
Fredrik Thulin <ft@it.su.se>
2006-06-13 10:07:29 +0000

    init
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@1417 e454d3dc-12ff-0310-9b8b-94264392205f

---
+++ src/include/event.hrl
@@ -0,0 +1,16 @@
+%%--------------------------------------------------------------------
+%% database_evendata.erl records
+%%--------------------------------------------------------------------
+-record(eventdata_dbe, {
+	  presentity,	%% term(), presentity this data belongs to ({user, User} | {address, AddressStr} | ...)
+	  etag,		%% term(), event package record reference
+	  expires,	%% never | integer(), util:timestamp() time when this record expires
+	  flags,	%% list() of {Key, Value} for future use
+	  data		%% term(), event package data
+	 }).
+
+%% Record for passing context information to event packages request/7 function
+-record(event_ctx, {
+	  sipuser,	%% undefined | string(), authenticated SIP user
+	  dialog_id	%% undefined | tuple() of arity 3, dialog identifier
+	 }).
