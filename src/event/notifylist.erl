7499f475b855d34e38f715933d9702720809936d
Fredrik Thulin <ft@it.su.se>
2006-09-14 07:49:13 +0000

    implement get_all_pids/0
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@1452 e454d3dc-12ff-0310-9b8b-94264392205f

--- src/event/notifylist.erl
+++ src/event/notifylist.erl
@@ -12,7 +12,8 @@
 -export([init/0,
 	 add/3,
 	 delete/3,
-	 lookup/2
+	 lookup/2,
+	 get_all_pids/0
 	]).
 
 
@@ -30,15 +31,22 @@ init() ->
 
 %% Returs : ok
 add(Presentity, PackageS, Pid) when is_tuple(Presentity), is_list(PackageS), is_pid(Pid) ->
-    true = ets:insert(?ETS_SUBSCRIPTIONS_TABLE, {Presentity, {PackageS, Pid}}),
+    Entry = {Presentity, {PackageS, Pid}},
+    true = ets:insert(?ETS_SUBSCRIPTIONS_TABLE, Entry),
     ok.
 
 %% Returs : ok
 delete(Presentity, PackageS, Pid) when is_tuple(Presentity), is_list(PackageS), is_pid(Pid) ->
-    true = ets:delete(?ETS_SUBSCRIPTIONS_TABLE, {Presentity, {PackageS, Pid}}),
+    Entry = {Presentity, {PackageS, Pid}},
+    true = ets:delete_object(?ETS_SUBSCRIPTIONS_TABLE, Entry),
     ok.
 
 %% Returns : list() of pid()
 lookup(Presentity, PackageFilter) when is_tuple(Presentity), is_list(PackageFilter) ->
     L = ets:lookup(?ETS_SUBSCRIPTIONS_TABLE, Presentity),
     [Pid || {_Id, {PackageS, Pid}} <- L, PackageS == PackageFilter].
+
+%% Returns : list() of pid()
+get_all_pids() ->
+    L = ets:tab2list(?ETS_SUBSCRIPTIONS_TABLE),
+    [Pid || {_Id, {PackageS, Pid}} <- L].
