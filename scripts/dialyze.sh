591e3b25ded261061b29bfda4a5e6abf3140b533
Fredrik Thulin <ft@it.su.se>
2008-11-16 14:38:35 +0000

    add functionality to show differences to last run
    git-svn-id: svn://anonsvn.it.su.se/yxa/branches/SU-trunk@1690 e454d3dc-12ff-0310-9b8b-94264392205f

--- scripts/dialyze.sh
+++ scripts/dialyze.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-params=""
+params="-Wunmatched_returns"
 
 if [ "x$1" = "x--src" ]; then
     params="$params --src"
@@ -20,6 +20,8 @@ includes=$(
 	    echo "-I ${dir}/src/$entry"
 	fi
     done
+    test -d ../build/src/eldap/ && echo "-I ../build/src/eldap/"
+    test -d ~/yxa/build/src/eldap/ && echo "-I `echo ~/yxa/build/src/eldap/`"
 )
 
 includes2=$(
@@ -37,8 +39,39 @@ dialyzer=$(which dialyzer)
 if [ -x "$dialyzer" ]; then
     echo $dialyzer $params
     echo ""
-    $dialyzer $params | grep -ve "^{local,.*calls missing or unexported.*local"
+
+    local_call_to_regexp="^local.*call.* missing or unexported.*local"
+
+    out_dir="$TMPDIR/yxa-dialyzer-output" 
+    if [ -d "$out_dir" ]; then
+	out_base=$(echo "out_$PWD/$dir" | sed -e 's!\./!!g' -e 's!/*$!!g' | tr / _)
+	out="$out_dir/${out_base}__`date +%Y-%m-%d_%Hh%Mm%Ss`"
+
+	previous_out=$(ls -1tr $out_dir/${out_base}__* 2>/dev/null | tail -1)
+	if [ "x$previous_out" != "x" ]; then
+	    echo "Previous output found : $previous_out"
+	else
+	    echo "No previous output found with this base ($out_base)"
+	fi
+
+	echo "Saving output in file '$out'"
+
+	$dialyzer $params | grep -vie "$local_call_to_regexp" | tee "$out"
+	grep -v "done in" "$out" > "$out.$$"
+	mv -f "$out.$$" "$out"
+    else
+	$dialyzer $params | grep -vie "$local_call_to_regexp"
+    fi
 else
     echo "No dialyzer in \$PATH"
     exit 1
+fi
+
+if [ -f "$previous_out" -a -f "$out" ]; then
+    echo ""
+    echo ""
+    echo "Diff to last output :"
+    diff -wu "$previous_out" "$out"
+    echo ""
+    echo ""
 fi
