fe5b64e9659e24dc7cae2c3bab349811f4ed0811
Fredrik Thulin <ft@it.su.se>
2009-03-25 10:32:55 +0000

    add first unit tests, copied from outgoingproxy
    git-svn-id: svn://anonsvn.it.su.se/yxa/trunk@1707 e454d3dc-12ff-0310-9b8b-94264392205f

--- src/incomingproxy_test.erl
+++ src/incomingproxy_test.erl
@@ -15,6 +15,7 @@
 %% Include files
 %%--------------------------------------------------------------------
 -include("siprecords.hrl").
+-include("sipsocket.hrl").
 
 %%====================================================================
 %% Test functions
@@ -34,6 +35,146 @@ test() ->
 -else.
 
 test() ->
+    autotest:mark(?LINE, "incomingproxy_test setup - 0"),
+
+    UserDb =
+	[{user, [
+		 {name, "autotest1"},
+		 {password, "secret"},
+		 {classes, [internal,national,mobile]},
+		 {addresses, ["sip:autotest1@example.org", "sip:234599@example.org"]}
+		]},
+	 {user, [
+		 {name, "autotest2"},
+		 {addresses, ["sip:autotest2@example.org"]}
+		]}
+
+	],
+    ok = sipuserdb_test:init(UserDb),
+
+    ExtraCfg = [
+		{userdb_modules,	[sipuserdb_test]},
+		{myhostnames,		["autotest.example.org"]}
+	       ],
+    yxa_test_config:init(incomingproxy, ExtraCfg),
+
+    ok = test_request(),
+
     ok.
+
+test_request() ->
+    autotest_util:store_unit_test_result(incomingproxy, testing_sippipe, {ok, self()}),
+    autotest_util:store_unit_test_result(transactionlayer, get_branch_from_handler, "test-branch"),
+
+    ok = test_route(),
+
+    ok.
+
+
+test_route() ->
+    autotest:mark(?LINE, "request/2 - Route - 0.0"),
+
+    phone:test_create_table(),
+    database_gruu:test_create_table(),
+
+    case mnesia:transaction(fun test_route2/0) of
+	{aborted, ok} ->
+	    ok;
+	{aborted, Res} ->
+	    {error, Res}
+    end.
+
+test_route2() ->
+    autotest:mark(?LINE, "request/2 - Route - 0.1"),
+    Location = sipurl:parse("sip:ua@192.0.2.1"),
+    RouteURL = sipurl:parse("sip:ua@192.0.2.66"),
+
+    {atomic, ok} = phone:insert_purge_phone("autotest1", [], static, util:timestamp() + 10,
+					    Location, "test callid", 1, ""),
+
+    YxaCtx1 = #yxa_ctx{thandler = transactionlayer:test_get_thandler_self(),
+		       origin   = #siporigin{proto = yxa_test,
+					     addr  = "192.0.2.11",
+					     port  = 50000
+					    }
+		      },
+
+    %% request(Request, YxaCtx)
+    %%--------------------------------------------------------------------
+    autotest:mark(?LINE, "request/2 - Route - 1.0"),
+    %% test request to registered location of one of our users, with Route header
+
+    Message1 =
+	"INVITE " ++ sipurl:print(Location) ++ " SIP/2.0\r\n"
+	"Via: SIP/2.0/YXA-TEST one.example.org\r\n"
+	"From: Test <sip:test@example.org;tag=abc>\r\n"
+	"To: Test <sip:test@example.org>\r\n"
+	"Route: <" ++ sipurl:print(RouteURL) ++ ">\r\n"
+	"\r\n",
+
+    Request1 = sippacket:parse(Message1, none),
+
+    autotest:mark(?LINE, "request/2 - Route - 1.1"),
+    ok = incomingproxy:request(Request1, YxaCtx1),
+
+    {Request1_Res, _YxaCtx1_Res11, Dst1_Res, []} = autotest_util:get_sippipe_result(),
+
+    autotest:mark(?LINE, "request/2 - Route - 1.2"),
+    %% verify results
+    #request{uri = Location} = Request1_Res,
+    ExpectedRoute1 = ["<" ++ sipurl:print(RouteURL) ++ ">"],
+    ExpectedRoute1 = keylist:fetch('route', Request1_Res#request.header),
+    route = Dst1_Res,
+
+
+    autotest:mark(?LINE, "request/2 - Route - 2.0"),
+    %% test BYE request with Route, can't be challenged (no ;lr)
+
+    Message2 =
+	"BYE sip:remote.example.net SIP/2.0\r\n"
+	"Via: SIP/2.0/YXA-TEST one.example.org\r\n"
+	"From: Test <sip:test@example.org;tab=bcd>\r\n"
+	"To: Test <sip:test@example.org>\r\n"
+	"Route: <sip:192.0.2.67>\r\n"
+	"\r\n",
+
+    Request2 = sippacket:parse(Message2, none),
+
+    autotest:mark(?LINE, "request/2 - Route - 2.1"),
+    ok = incomingproxy:request(Request2, YxaCtx1),
+
+    {Request2_Res, _YxaCtx1_Res21, Dst2_Res, []} = autotest_util:get_sippipe_result(),
+
+    autotest:mark(?LINE, "request/2 - Route - 2.2"),
+    %% verify results. the push-requri-to-Route for Route header without ;lr
+    %% is done in sippipe, so it isn't visible here
+    "sip:remote.example.net" = sipurl:print(Request2_Res#request.uri),
+    route = Dst2_Res,
+    ["<sip:192.0.2.67>"] = keylist:fetch('route', Request2_Res#request.header),
+
+
+    autotest:mark(?LINE, "request/2 - Route - 3.0"),
+    %% test non-BYE with remote Route destination, and remote Request-URI - should be challenged
+
+    Message3 =
+	"TEST sip:remote.example.net SIP/2.0\r\n"
+	"Via: SIP/2.0/YXA-TEST one.example.org\r\n"
+	"From: Test <sip:test@example.org;tab=bcd>\r\n"
+	"To: Test <sip:test@example.org>\r\n"
+	"Route: <sip:192.0.2.67>\r\n"
+	"\r\n",
+
+    Request3 = sippacket:parse(Message3, none),
+
+    autotest:mark(?LINE, "request/2 - Route - 3.1"),
+    ok = incomingproxy:request(Request3, YxaCtx1),
+
+    autotest:mark(?LINE, "request/2 - Route - 3.2"),
+    %% verify results
+    {407, "Proxy Authentication Required", [{"Proxy-Authenticate", _}], <<>>} =
+	autotest_util:get_created_response(),
+
+    mnesia:abort(ok).
+
 
 -endif.
